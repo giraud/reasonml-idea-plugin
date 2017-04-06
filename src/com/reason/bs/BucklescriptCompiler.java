9c12ef07c4a57dcf9c4f87b1538baa7839d6a86a
hgiraud <giraud.contact@yahoo.fr>
2017-04-06 11:00:34 +0200

    add a console view to show bucklescript output

--- src/com/reason/bs/BucklescriptCompiler.java
+++ src/com/reason/bs/BucklescriptCompiler.java
@@ -1,5 +1,9 @@
 package com.reason.bs;
 
+import com.intellij.execution.ExecutionException;
+import com.intellij.execution.configurations.GeneralCommandLine;
+import com.intellij.execution.process.KillableColoredProcessHandler;
+import com.intellij.execution.process.ProcessHandler;
 import com.intellij.notification.NotificationType;
 import com.intellij.notification.Notifications;
 import com.intellij.openapi.components.AbstractProjectComponent;
@@ -7,16 +11,11 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.reason.ide.ReasonMLNotification;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class BucklescriptCompiler extends AbstractProjectComponent {
 
-    private Process bsb;
-    private Thread streamListener;
+    private KillableColoredProcessHandler bsb;
 
     protected BucklescriptCompiler(Project project) {
         super(project);
@@ -42,6 +41,16 @@ public class BucklescriptCompiler extends AbstractProjectComponent {
             return;
         }
 
+        GeneralCommandLine commandLine = new GeneralCommandLine(bsbBinary.getCanonicalPath(), "-make-world", "-w");
+        commandLine.setWorkDirectory(baseDir.getCanonicalPath());
+
+        try {
+            this.bsb = new KillableColoredProcessHandler(commandLine);
+        } catch (ExecutionException e) {
+            Notifications.Bus.notify(new ReasonMLNotification("Bsb", "Can't run bsb\n" + e.getMessage(), NotificationType.ERROR));
+        }
+
+/*
         ProcessBuilder processBuilder = new ProcessBuilder(bsbBinary.getCanonicalPath(), "-make-world", "-w")
                 .directory(new File(baseDir.getCanonicalPath()))
                 .redirectErrorStream(true);
@@ -98,15 +107,23 @@ public class BucklescriptCompiler extends AbstractProjectComponent {
         } catch (IOException e) {
             Notifications.Bus.notify(new ReasonMLNotification("Bsb", "Can't run bsb\n" + e.getMessage(), NotificationType.ERROR));
         }
+*/
     }
 
     @Override
     public void projectClosed() {
-        if (this.bsb != null && this.bsb.isAlive()) {
-            this.streamListener.interrupt();
-            this.bsb.destroyForcibly();
+        if (this.bsb != null) {
+            this.bsb.killProcess();
             this.bsb = null;
         }
     }
 
+    // Wait for the toolwindow to be ready before starting the process
+    void startNotify() {
+        this.bsb.startNotify();
+    }
+
+    ProcessHandler getHandler() {
+        return this.bsb;
+    }
 }
