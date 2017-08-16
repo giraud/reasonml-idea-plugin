13c15d7a5ca55bbc04c5ce23fe870740bafb7472
giraud <hgiraud@axway.com>
2017-08-16 22:28:05 +0200

    chore: remove a stdout

--- src/com/reason/ide/ReasonMLCompletionContributor.java
+++ src/com/reason/ide/ReasonMLCompletionContributor.java
@@ -33,7 +33,7 @@ public class ReasonMLCompletionContributor extends CompletionContributor {
                 MerlinPosition position = lineNumbering.offsetToPosition(parameters.getOffset());
                 MerlinCompletion completion = merlin.completions(originalFile.getName(), suitablePrefix, position);
 
-                System.out.println("Completion prefix: " + suitablePrefix);
+                // System.out.println("Completion prefix: " + suitablePrefix);
                 for (MerlinCompletionEntry entry : completion.entries) {
                     resultSet.addElement(LookupElementBuilder.
                             create(entry.name).
