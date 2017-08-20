a501bdfcf5a23a089c293e3373077982eeba8cdb
giraud <hgiraud@axway.com>
2017-08-20 17:08:28 +0200

    Fix module folding

--- src/com/reason/ide/folding/RmlFoldingBuilder.java
+++ src/com/reason/ide/folding/RmlFoldingBuilder.java
@@ -4,7 +4,6 @@ import com.intellij.lang.ASTNode;
 import com.intellij.lang.folding.FoldingBuilderEx;
 import com.intellij.lang.folding.FoldingDescriptor;
 import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.tree.IElementType;
 import com.intellij.psi.util.PsiTreeUtil;
@@ -27,29 +26,24 @@ public class RmlFoldingBuilder extends FoldingBuilderEx {
             IElementType elementType = element.getNode().getElementType();
             if (COMMENT.equals(elementType)) {
                 descriptors.add(fold(element));
-            }
-            else if (TYPE_EXPRESSION.equals(elementType)) {
-                ReasonMLScopedExpr scopedExpression = ((ReasonMLType) element).getScopedExpression();
-                if (scopedExpression != null) {
-                    descriptors.add(fold(scopedExpression));
-                }
-            }
-            else if (LET_EXPRESSION.equals(elementType)) {
+            } else if (TYPE_EXPRESSION.equals(elementType)) {
+                foldType(descriptors, (ReasonMLType) element);
+            } else if (LET_EXPRESSION.equals(elementType)) {
                 foldLet(descriptors, (ReasonMLLet) element);
-            }
-            else if (MODULE.equals(elementType)) {
-                PsiElement lBrace = RmlPsiTreeUtil.getNextSiblingOfType(element, LBRACE);
-                PsiElement rBrace = RmlPsiTreeUtil.getNextSiblingOfType(lBrace, RBRACE);
-                if (lBrace != null && rBrace != null) {
-                    FoldingDescriptor fold = foldBetween(element, lBrace, rBrace, 5);
-                    if (fold != null)
-                        descriptors.add(fold);
-                }
+            } else if (MODULE_EXPRESSION.equals(elementType)) {
+                foldModule(descriptors, (ReasonMLModule) element);
             }
             return true;
         });
 
         return descriptors.toArray(new FoldingDescriptor[descriptors.size()]);
+    }
+
+    private void foldType(List<FoldingDescriptor> descriptors, ReasonMLType typeExpression) {
+        ReasonMLScopedExpr scopedExpression = typeExpression.getScopedExpression();
+        if (scopedExpression != null) {
+            descriptors.add(fold(scopedExpression));
+        }
     }
 
     private void foldLet(List<FoldingDescriptor> descriptors, ReasonMLLet letExpression) {
@@ -62,6 +56,20 @@ public class RmlFoldingBuilder extends FoldingBuilderEx {
                 descriptors.add((fold(letBinding)));
             }
         }
+    }
+
+    private void foldModule(List<FoldingDescriptor> descriptors, ReasonMLModule module) {
+        ReasonMLScopedExpr moduleBody = module.getModuleBody();
+        if (moduleBody != null) {
+            descriptors.add(fold(moduleBody));
+        }
+        // PsiElement lBrace = RmlPsiTreeUtil.getNextSiblingOfType(element, LBRACE);
+        // PsiElement rBrace = RmlPsiTreeUtil.getNextSiblingOfType(lBrace, RBRACE);
+        // if (lBrace != null && rBrace != null) {
+        //     FoldingDescriptor fold = foldBetween(element, lBrace, rBrace, 5);
+        //     if (fold != null)
+        //         descriptors.add(fold);
+        // }
     }
 
     @Nullable
@@ -78,14 +86,14 @@ public class RmlFoldingBuilder extends FoldingBuilderEx {
         return false;
     }
 
-    @Nullable
-    private FoldingDescriptor foldBetween(PsiElement element, PsiElement left, PsiElement right, int minWidth) {
-        if (right.getTextOffset() - left.getTextOffset() < minWidth) {
-            return null;
-        }
-        TextRange range = new TextRange(left.getTextOffset() + 1, right.getTextOffset());
-        return new FoldingDescriptor(element, range);
-    }
+    // @Nullable
+    // private FoldingDescriptor foldBetween(PsiElement element, PsiElement left, PsiElement right, int minWidth) {
+    //     if (right.getTextOffset() - left.getTextOffset() < minWidth) {
+    //         return null;
+    //     }
+    //     TextRange range = new TextRange(left.getTextOffset() + 1, right.getTextOffset());
+    //     return new FoldingDescriptor(element, range);
+    // }
 
     private FoldingDescriptor fold(PsiElement element) {
         return new FoldingDescriptor(element, element.getTextRange());
