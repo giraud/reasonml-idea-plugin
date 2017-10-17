85c629d751caaae8f44e8508964e3596bea3cdb8
giraud <giraud.contact@yahoo.fr>
2017-10-17 09:51:51 +0200

    Refactoring bsb code

--- src/com/reason/bs/BucklescriptToolWindowFactory.java
+++ src/com/reason/bs/BucklescriptToolWindowFactory.java
@@ -1,7 +1,5 @@
-package com.reason.bs.console;
+package com.reason.bs;
 
-import com.intellij.execution.filters.TextConsoleBuilderFactory;
-import com.intellij.execution.impl.ConsoleViewImpl;
 import com.intellij.execution.process.ProcessHandler;
 import com.intellij.openapi.actionSystem.ActionToolbar;
 import com.intellij.openapi.components.ServiceManager;
@@ -12,9 +10,7 @@ import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
-import com.intellij.ui.content.ContentManager;
-import com.reason.bs.BsbOutputListener;
-import com.reason.bs.BucklescriptCompiler;
+import com.reason.bs.console.BucklescriptConsole;
 import org.jetbrains.annotations.NotNull;
 
 import static com.intellij.execution.ui.ConsoleViewContentType.ERROR_OUTPUT;
@@ -26,20 +22,17 @@ public class BucklescriptToolWindowFactory implements ToolWindowFactory, DumbAwa
         BucklescriptCompiler bsc = ServiceManager.getService(project, BucklescriptCompiler.class);
 
         SimpleToolWindowPanel panel = new SimpleToolWindowPanel(false, true);
+        BucklescriptConsole console = new BucklescriptConsole(project);
+        ActionToolbar toolbar = console.createToolbar(bsc);
 
-        ConsoleViewImpl console = (ConsoleViewImpl) TextConsoleBuilderFactory.getInstance().createBuilder(project).getConsole();
         panel.setContent(console.getComponent());
-
-        ActionToolbar toolbar = BucklescriptConsole.createToolbar(console, bsc);
         panel.setToolbar(toolbar.getComponent());
 
         Content content = ContentFactory.SERVICE.getInstance().createContent(panel, "", true);
-
-        ContentManager contentManager = toolWindow.getContentManager();
-        contentManager.addContent(content);
+        toolWindow.getContentManager().addContent(content);
 
         // Start compiler
-        bsc.addListener(new BsbOutputListener(new ConsoleBusImpl(console, toolbar), project));
+        bsc.addListener(new BsbOutputListener(console, project));
         ProcessHandler handler = bsc.getHandler();
         if (handler == null) {
             console.print("Bsb not found, check the event logs.", ERROR_OUTPUT);
@@ -47,23 +40,5 @@ public class BucklescriptToolWindowFactory implements ToolWindowFactory, DumbAwa
             console.attachToProcess(handler);
         }
         bsc.startNotify();
-    }
-
-    static class ConsoleBusImpl implements ConsoleBus {
-
-        private final ConsoleViewImpl m_console;
-        private final ActionToolbar m_toolbar;
-
-        ConsoleBusImpl(ConsoleViewImpl console, ActionToolbar toolbar) {
-            m_console = console;
-            m_toolbar = toolbar;
-        }
-
-        @Override
-        public void processTerminated() {
-            m_console.print("\nProcess has terminated, fix the problem before restarting it.", ERROR_OUTPUT);
-            StartAction startAction = (StartAction) m_toolbar.getActions().get(2);
-            startAction.setEnable(true);
-        }
     }
 }
--- src/com/reason/bs/console/BucklescriptConsole.java
+++ src/com/reason/bs/console/BucklescriptConsole.java
@@ -1,22 +1,41 @@
 package com.reason.bs.console;
 
 import com.intellij.execution.impl.ConsoleViewImpl;
-import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.ActionToolbar;
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
 import com.intellij.openapi.editor.actions.ScrollToTheEndToolbarAction;
+import com.intellij.openapi.project.Project;
 import com.reason.bs.BucklescriptCompiler;
+import org.jetbrains.annotations.NotNull;
 
-public class BucklescriptConsole {
+import static com.intellij.execution.ui.ConsoleViewContentType.ERROR_OUTPUT;
 
-    public static ActionToolbar createToolbar(ConsoleViewImpl console, BucklescriptCompiler bsc) {
+public class BucklescriptConsole extends ConsoleViewImpl implements ConsoleBus {
+
+    private ActionToolbar m_toolbar;
+
+    public BucklescriptConsole(@NotNull Project project) {
+        super(project, true);
+    }
+
+    public ActionToolbar createToolbar(BucklescriptCompiler bsc) {
         DefaultActionGroup group = new DefaultActionGroup();
-        group.add(new ScrollToTheEndToolbarAction(console.getEditor()));
-        group.add(new ClearLogAction(console));
-        group.add(new StartAction(console, bsc));
+        group.add(new ScrollToTheEndToolbarAction(this.getEditor()));
+        group.add(new ClearLogAction(this));
+        group.add(new StartAction(this, bsc));
 
-        ActionToolbar toolbar = ActionManager.getInstance().createActionToolbar("left", group, false);
-        toolbar.setTargetComponent(console.getComponent());
+        m_toolbar = ActionManager.getInstance().createActionToolbar("left", group, false);
+        m_toolbar.setTargetComponent(this.getComponent());
 
-        return toolbar;
+        return m_toolbar;
+    }
+
+    @Override
+    public void processTerminated() {
+        print("\nProcess has terminated, fix the problem before restarting it.", ERROR_OUTPUT);
+        StartAction startAction = (StartAction) m_toolbar.getActions().get(2);
+        startAction.setEnable(true);
     }
 
 }
