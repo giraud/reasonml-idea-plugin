46250f6aee754422272fbd0a77619de3d8088be3
giraud <giraud.contact@yahoo.fr>
2017-12-19 15:15:24 +0100

    Fix Nullable import

--- src/main/java/com/reason/lang/core/psi/PsiModule.java
+++ src/main/java/com/reason/lang/core/psi/PsiModule.java
@@ -3,8 +3,8 @@ package com.reason.lang.core.psi;
 import com.intellij.psi.NavigatablePsiElement;
 import com.intellij.psi.StubBasedPsiElement;
 import com.reason.lang.core.stub.ModuleStub;
+import org.jetbrains.annotations.Nullable;
 
-import javax.annotation.Nullable;
 
 public interface PsiModule extends PsiNamedElement, NavigatablePsiElement, PsiStructuredElement, StubBasedPsiElement<ModuleStub> {
     @Nullable
