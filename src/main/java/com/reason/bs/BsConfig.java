6ce432ef1422a4550e86519db5020b7d91743787
giraud <giraud.contact@yahoo.fr>
2018-03-21 13:36:59 +0100

    Read namespace value

--- src/main/java/com/reason/bs/BsConfig.java
+++ src/main/java/com/reason/bs/BsConfig.java
@@ -11,15 +11,21 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class BsConfig {
+
     private static Pattern DEPS_REGEXP = Pattern.compile(".*\"bs-dependencies\":\\s*\\[(.*?)].*");
+    private static Pattern NAME_REGEXP = Pattern.compile(".*\"name\":\\s*\"([^\"]*?)\".*");
+
     private static String[] PERVASIVES = new String[]{
             // all files but the ones with _ ?
             "bs-platform/lib/ocaml/js.mli",
     };
 
+    private final String m_name;
     private final String[] m_deps;
 
-    private BsConfig(@Nullable String[] deps) {
+    private BsConfig(String name, @Nullable String[] deps) {
+        m_name = name;
+
         if (deps == null) {
             m_deps = new String[PERVASIVES.length];
             System.arraycopy(PERVASIVES, 0, m_deps, 0, PERVASIVES.length);
@@ -37,24 +43,41 @@ class BsConfig {
                 // Quick and dirty code to read json values from bsconfig
                 StringBuilder content = new StringBuilder();
                 reader.lines().forEach(line -> content.append(line.trim()));
-                // extract bs dependencies
-                Matcher matcher = DEPS_REGEXP.matcher(content.toString());
 
-                String[] deps = null;
-                if (matcher.matches()) {
-                    String[] tokens = matcher.group(1).split(",");
-                    deps = new String[tokens.length];
-                    for (int i = 0; i < tokens.length; i++) {
-                        String token = tokens[i].trim();
-                        deps[i] = token.substring(1, token.length() - 1) + "/lib";
-                    }
-                }
+                String name = readName(content);
+                String[] deps = readDependencies(content);
 
-                return new BsConfig(deps);
+                return new BsConfig(name, deps);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static String[] readDependencies(StringBuilder content) {
+        String[] result = null;
+
+        Matcher matcher = DEPS_REGEXP.matcher(content.toString());
+        if (matcher.matches()) {
+            String[] tokens = matcher.group(1).split(",");
+            result = new String[tokens.length];
+            for (int i = 0; i < tokens.length; i++) {
+                String token = tokens[i].trim();
+                result[i] = token.substring(1, token.length() - 1) + "/lib";
+            }
+        }
+        return result;
+    }
+
+    private static String readName(StringBuilder content) {
+        String result = null;
+
+        Matcher matcher = NAME_REGEXP.matcher(content.toString());
+        if (matcher.matches()) {
+            result = matcher.group(1);
+        }
+
+        return result;
     }
 
     boolean accept(@Nullable String canonicalPath) {
