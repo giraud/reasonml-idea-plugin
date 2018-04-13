360271df40f0278ffffd297199b25ed7975fe825
giraud <giraud.contact@yahoo.fr>
2018-04-13 13:31:57 +0200

    fix problems

--- src/main/java/com/reason/lang/core/psi/impl/RmlElementFactory.java
+++ src/main/java/com/reason/lang/core/psi/impl/RmlElementFactory.java
@@ -3,30 +3,33 @@ package com.reason.lang.core.psi.impl;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFileFactory;
-import com.reason.ide.files.RmlFile;
-import com.reason.ide.files.RmlFileType;
+import com.reason.ide.files.FileBase;
 import com.reason.lang.RmlLanguage;
+import com.reason.lang.core.psi.PsiModule;
 import com.reason.lang.core.psi.PsiType;
-import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 class RmlElementFactory {
     private RmlElementFactory() {
     }
 
+    @Nullable
     static PsiElement createModuleName(Project project, String name) {
-        RmlFile dummyFile = createFileFromText(project, "module " + name + " = {};");
-        return dummyFile.asModule().getModules().iterator().next().getNameIdentifier();
+        PsiModule dummyModule = createFileFromText(project, "module " + name + " = {};").asModule();
+        if (dummyModule != null) {
+            return dummyModule.getModules().iterator().next().getNameIdentifier();
+        }
+        return null;
     }
 
-    public static PsiElement createTypeName(Project project, String name) {
-        RmlFile dummyFile = createFileFromText(project, "type " + name + ";");
+    static PsiElement createTypeName(Project project, String name) {
+        FileBase dummyFile = createFileFromText(project, "type " + name + ";");
         return ((PsiType) dummyFile.getFirstChild()).getNameIdentifier();
     }
 
     @NotNull
-    private static RmlFile createFileFromText(@NotNull Project project, @NotNull String text) {
-        @NonNls String filename = "dummy." + RmlFileType.INSTANCE.getDefaultExtension();
-        return (RmlFile) PsiFileFactory.getInstance(project).createFileFromText(filename, RmlLanguage.INSTANCE, text);
+    private static FileBase createFileFromText(@NotNull Project project, @NotNull String text) {
+        return (FileBase) PsiFileFactory.getInstance(project).createFileFromText("Dummy.re", RmlLanguage.INSTANCE, text);
     }
 }
