38790e470a2eaa4c8a0344c1d351c9b4e55712a9
giraud <giraud.contact@yahoo.fr>
2018-05-11 17:19:03 +0200

    woa 1mn is too much

--- src/main/java/com/reason/Streams.java
+++ src/main/java/com/reason/Streams.java
@@ -4,11 +4,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 
 public class Streams {
+    private Streams() {
+    }
+
     public static void waitUntilReady(BufferedReader reader, BufferedReader errorReader) throws IOException {
         long start = System.currentTimeMillis();
         boolean isReady = reader.ready() || errorReader.ready();
         while (!isReady) {
-            if (60000 < (System.currentTimeMillis() - start)) {
+            if (200 < (System.currentTimeMillis() - start)) {
                 // max 1s
                 isReady = true;
             } else {
