55551806166084cbb5462000b1b91a23a0d2dc3a
giraud <giraud.contact@yahoo.fr>
2018-09-21 15:24:44 +0200

    Revert type annotation, only color type argument

--- src/main/java/com/reason/ide/highlight/ORSyntaxAnnotator.java
+++ src/main/java/com/reason/ide/highlight/ORSyntaxAnnotator.java
@@ -6,11 +6,9 @@ import com.intellij.lang.annotation.Annotator;
 import com.intellij.openapi.editor.colors.EditorColorsManager;
 import com.intellij.openapi.editor.colors.EditorColorsScheme;
 import com.intellij.openapi.editor.markup.TextAttributes;
-import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.tree.IElementType;
 import com.reason.lang.core.psi.PsiInterpolation;
-import com.reason.lang.core.psi.PsiType;
 import com.reason.lang.core.psi.PsiUpperSymbol;
 import com.reason.lang.core.type.ORTypes;
 import org.jetbrains.annotations.NotNull;
@@ -29,23 +27,7 @@ public abstract class ORSyntaxAnnotator implements Annotator {
         EditorColorsScheme globalScheme = EditorColorsManager.getInstance().getGlobalScheme();
         IElementType elementType = element.getNode().getElementType();
 
-        if (element instanceof PsiType) {
-            TextAttributes scheme = globalScheme.getAttributes(ORSyntaxHighlighter.TYPE_ARGUMENT_);
-            String name = ((PsiType) element).getName();
-            if (name != null && !name.isEmpty()) {
-                // find all occurrences of name
-                int nameLength = name.length();
-                String text = element.getText();
-                int namePos = text.indexOf(name);
-                while (namePos >= 0) {
-                    TextRange range = TextRange.from(element.getTextOffset() + namePos, nameLength);
-                    Annotation infoAnnotation = holder.createInfoAnnotation(range, null);
-                    infoAnnotation.setEnforcedTextAttributes(ERASE_MARKER);
-                    infoAnnotation.setEnforcedTextAttributes(scheme);
-                    namePos = text.indexOf(name, namePos + nameLength);
-                }
-            }
-        } else if (elementType == m_types.UPPER_SYMBOL) {
+        if (elementType == m_types.UPPER_SYMBOL) {
             PsiUpperSymbol symbol = (PsiUpperSymbol) element;
             TextAttributes colorAttribute = globalScheme.getAttributes(symbol.isVariant() ? ORSyntaxHighlighter.VARIANT_NAME_ : ORSyntaxHighlighter.MODULE_NAME_);
             Annotation annotation = holder.createInfoAnnotation(element, null);
