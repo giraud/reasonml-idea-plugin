730d6d5b5dd11f8098632fd917386ea4b01a55c3
giraud <giraud.contact@yahoo.fr>
2018-10-23 23:43:30 +0200

    use disposers for editor/document listener

--- src/com/reason/ide/ORFileEditorListener.java
+++ src/com/reason/ide/ORFileEditorListener.java
@@ -6,9 +6,8 @@ import com.intellij.openapi.editor.event.DocumentListener;
 import com.intellij.openapi.fileEditor.*;
 import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.util.messages.MessageBusConnection;
-import com.reason.build.Compiler;
 import com.reason.build.CompilerManager;
 import com.reason.build.console.CliType;
 import com.reason.ide.files.FileHelper;
@@ -25,52 +24,28 @@ import java.util.List;
  * Listen to editor events and query merlin for types when editor get the focus.
  */
 public class ORFileEditorListener implements FileEditorManagerListener {
-//    private static final Key<ListenerInfo<PropertyChangeListener>> PROP_LISTENER = Key.create("reasonml.propinfo");
 
     private final Project m_project;
     private final List<VirtualFile> m_openedFiles = new ArrayList<>();
 
-    //    static class ListenerInfo<T extends EventListener> {
-//        private final T m_listener;
-//
-//        ListenerInfo(T documentListener) {
-//            m_listener = documentListener;
-//        }
-//
-//        T getListener() {
-//            return m_listener;
-//        }
-//    }
     ORFileEditorListener(Project project) {
         m_project = project;
     }
 
     @Override
     public void fileOpened(@NotNull FileEditorManager source, @NotNull VirtualFile file) {
-        FileEditor selectedEditor = source.getSelectedEditor(file);
-        Document document = FileDocumentManager.getInstance().getDocument(file);
-        if (selectedEditor != null && document != null) {
-            final PropertyChangeListener propertyChangeListener = new ORPropertyChangeListener(file);
-            final DocumentListener documentListener = new ORDocumentEventListener();
-
-            selectedEditor.addPropertyChangeListener(propertyChangeListener);
-            document.addDocumentListener(documentListener, m_project);
-
-            MessageBusConnection messageBus = m_project.getMessageBus().connect(m_project);
-            messageBus.subscribe(FileEditorManagerListener.Before.FILE_EDITOR_MANAGER, new FileEditorManagerListener.Before.Adapter() {
-                @Override
-                public void beforeFileClosed(@NotNull FileEditorManager source, @NotNull VirtualFile file) {
-                    selectedEditor.removePropertyChangeListener(propertyChangeListener);
-//                    Document document = FileDocumentManager.getInstance().getDocument(file);
-//                    if (document != null) {
-//                        document.removeDocumentListener(documentListener);
-//                    }
-                }
-            });
-        }
-
         FileType fileType = file.getFileType();
         if (FileHelper.isReason(fileType) || FileHelper.isOCaml(fileType)) {
+            FileEditor selectedEditor = source.getSelectedEditor(file);
+            Document document = FileDocumentManager.getInstance().getDocument(file);
+            if (selectedEditor != null && document != null) {
+                PropertyChangeListener propertyChangeListener = new ORPropertyChangeListener(file);
+                selectedEditor.addPropertyChangeListener(propertyChangeListener);
+                Disposer.register(selectedEditor, () -> selectedEditor.removePropertyChangeListener(propertyChangeListener));
+
+                document.addDocumentListener(new ORDocumentEventListener(), selectedEditor);
+            }
+
             m_openedFiles.add(file);
         }
     }
