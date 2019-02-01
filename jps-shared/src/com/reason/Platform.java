547214f79aa52e9b2bdb3fb7be5fce7e6043f481
giraud <giraud.contact@yahoo.fr>
2019-02-01 23:29:00 +0100

    use rincewind 0.4

--- jps-shared/src/com/reason/Platform.java
+++ jps-shared/src/com/reason/Platform.java
@@ -16,7 +16,7 @@ import java.util.Map;
 public class Platform {
 
     public static final String OCAML_VERSION = "4.02";
-    public static final String RINCEWIND_VERSION = "0.4-dev";
+    public static final String RINCEWIND_VERSION = "0.4";
 
     private static final Map<Project, VirtualFile> m_baseDirs = new HashMap<>();
 
