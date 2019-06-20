bd5420b78788e2a411c4d31bf9b2edb31b0e2fc1
Hervé Giraud <giraud.contact@yahoo.fr>
2019-06-20 16:17:11 +0200

    fix test

--- tests/com/reason/ide/FindLIdentUsagesTest.java
+++ tests/com/reason/ide/FindLIdentUsagesTest.java
@@ -4,25 +4,26 @@ import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCa
 import com.intellij.usageView.UsageInfo;
 
 import java.util.Collection;
+import java.util.List;
 
 @SuppressWarnings("ConstantConditions")
 public class FindLIdentUsagesTest extends LightPlatformCodeInsightFixtureTestCase {
 
     public void testFromModule() {
-        myFixture.configureByText("A.re", "let x<caret> = 1;");
-        myFixture.configureByText("B.re", "let y = A.x + 2;");
+        myFixture.configureByText("FLIA.re", "let x<caret> = 1;");
+        myFixture.configureByText("FLIB.re", "let y = FLIA.x + 2;");
 
-        Collection<UsageInfo> usages = myFixture.testFindUsages("A.re");
-        assertSize(1, usages);
+        Collection<UsageInfo> usages = myFixture.testFindUsages("FLIA.re");
+        assertSize(2, usages);
 
     }
 
     public void testSameModule() {
-        myFixture.configureByText("A.re", "let x<caret> = 1; let y = x + 1;");
+        myFixture.configureByText("FLIC.re", "let x<caret> = 1; let y = x + 1;");
 
-        Collection<UsageInfo> usages = myFixture.testFindUsages("A.re");
-        assertSize(1, usages);
-        UsageInfo usageInfo = usages.iterator().next();
+        List<UsageInfo> usages = (List<UsageInfo>) myFixture.testFindUsages("FLIC.re");
+        assertSize(2, usages);
+        UsageInfo usageInfo = usages.get(1);
         assertEquals("x + 1", usageInfo.getElement().getParent().getText());
     }
 
