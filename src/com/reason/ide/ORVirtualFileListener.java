a027e809ea95fc8a0710517426cfb01b53cfcd39
Hervé Giraud <giraud.contact@yahoo.fr>
2019-12-11 13:45:26 +0100

    193 - #199, use an asyncFileListener, delete ORProjectTracker

--- src/com/reason/ide/ORVirtualFileListener.java
+++ src/com/reason/ide/ORVirtualFileListener.java
@@ -1,74 +1,63 @@
 package com.reason.ide;
 
+import java.util.*;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import com.intellij.json.JsonFileType;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleUtil;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.*;
+import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.vfs.AsyncFileListener;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.newvfs.events.VFileContentChangeEvent;
+import com.intellij.openapi.vfs.newvfs.events.VFileEvent;
+import com.intellij.util.containers.ContainerUtil;
 import com.reason.hints.InsightManager;
 import com.reason.ide.files.CmtFileType;
 import com.reason.ide.hints.CmtFileListener;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Listener that detects all modifications on project files
  */
-class ORVirtualFileListener implements VirtualFileListener {
+class ORVirtualFileListener implements AsyncFileListener {
 
-    @NotNull
-    private final Project m_project;
-
-    ORVirtualFileListener(@NotNull Project project) {
-        m_project = project;
-    }
-
+    @Nullable
     @Override
-    public void propertyChanged(@NotNull VirtualFilePropertyEvent event) {
-    }
-
-    @Override
-    public void contentsChanged(@NotNull VirtualFileEvent event) {
-        VirtualFile file = event.getFile();
-        FileType fileType = file.getFileType();
-
-        if (fileType instanceof JsonFileType) {
-            if (file.getName().equals("bsconfig.json")) {
-                ServiceManager.getService(m_project, InsightManager.class).downloadRincewindIfNeeded(file);
+    public ChangeApplier prepareChange(@NotNull List<? extends VFileEvent> events) {
+        List<? extends VFileEvent> relevantEvents = ContainerUtil.filter(events, this::isRelevantEvent);
+        return relevantEvents.isEmpty() ? null : new ChangeApplier() {
+            @Override
+            public void afterVfsChange() {
+                for (VFileEvent event : relevantEvents) {
+                    VirtualFile file = event.getFile();
+                    FileType fileType = file == null ? null : file.getFileType();
+                    if (fileType != null) {
+                        for (Project project : ProjectManager.getInstance().getOpenProjects()) {
+                            Module module = ModuleUtil.findModuleForFile(file, project);
+                            if (module != null) {
+                                if (fileType instanceof CmtFileType) {
+                                    ServiceManager.getService(project, CmtFileListener.class).onChange(file);
+                                } else if (fileType instanceof JsonFileType && "bsconfig.json".equals(file.getName())) {
+                                    ServiceManager.getService(project, InsightManager.class).downloadRincewindIfNeeded(file);
+                                }
+                            }
+                        }
+                    }
+                }
             }
-        } else if (fileType instanceof CmtFileType) {
-            ServiceManager.getService(m_project, CmtFileListener.class).onChange(file);
+        };
+    }
+
+    private boolean isRelevantEvent(@NotNull VFileEvent event) {
+        if (event instanceof VFileContentChangeEvent) {
+            VirtualFile file = event.getFile();
+            FileType fileType = file == null ? null : file.getFileType();
+            return fileType instanceof CmtFileType || (fileType instanceof JsonFileType && "bsconfig.json".equals(file.getName()));
         }
+        return false;
     }
 
-    @Override
-    public void fileCreated(@NotNull VirtualFileEvent event) {
-    }
-
-    @Override
-    public void fileDeleted(@NotNull VirtualFileEvent event) {
-    }
-
-    @Override
-    public void fileMoved(@NotNull VirtualFileMoveEvent event) {
-    }
-
-    @Override
-    public void fileCopied(@NotNull VirtualFileCopyEvent event) {
-    }
-
-    @Override
-    public void beforePropertyChange(@NotNull VirtualFilePropertyEvent event) {
-    }
-
-    @Override
-    public void beforeContentsChange(@NotNull VirtualFileEvent event) {
-    }
-
-    @Override
-    public void beforeFileDeletion(@NotNull VirtualFileEvent event) {
-    }
-
-    @Override
-    public void beforeFileMovement(@NotNull VirtualFileMoveEvent event) {
-    }
 }
