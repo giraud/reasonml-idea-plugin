7bd7ea73edf0d26ed3bb2cdc1c17f64143babd28
John Pucciarelli <john.pucciarelli@gmail.com>
2020-04-19 18:16:36 -0400

    Created dune constants file.

---
+++ src/com/reason/dune/DuneConstants.java
@@ -0,0 +1,13 @@
+package com.reason.dune;
+
+public class DuneConstants {
+
+    public static final String DUNE_PROJECT_FILENAME = "dune-project";
+
+    public static final String DUNE_FILENAME = "dune";
+
+    public static final String LEGACY_JBUILDER_FILENAME = "jbuilder";
+
+    private DuneConstants() {}
+
+}
