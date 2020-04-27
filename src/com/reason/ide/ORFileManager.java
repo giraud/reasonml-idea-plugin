be304b7ded88677038960d6a93a9e9a80cadc0de
Pooch <john.pucciarelli@gmail.com>
2020-04-27 02:59:17 -0400

    #232 - Removed unused CmtFileListener.

--- src/com/reason/ide/ORFileManager.java
+++ src/com/reason/ide/ORFileManager.java
@@ -9,22 +9,18 @@ import com.intellij.psi.search.FilenameIndex;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.reason.Joiner;
 import com.reason.Log;
-import com.reason.Platform;
 import com.reason.StringUtil;
 import com.reason.bs.Bucklescript;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.io.File;
-import java.nio.file.FileSystems;
 import java.nio.file.Path;
 
 public class ORFileManager {
 
     private static final Log LOG = Log.create("manager.file");
 
-    private ORFileManager() {
-    }
+    private ORFileManager() {}
 
     @Nullable
     public static PsiFile findCmtFileFromSource(@NotNull Project project, @NotNull String filenameWithoutExtension) {
@@ -61,56 +57,5 @@ public class ORFileManager {
         }
         int dotPos = sourcePath.lastIndexOf(".");
         return 0 <= dotPos ? sourcePath.substring(0, dotPos) + ".re" : sourcePath;
-    }
-
-    @Nullable
-    public static VirtualFile toSource(@NotNull Project project, @NotNull VirtualFile cmxFile, @NotNull Path relativeCmi) {
-        String relativeSource = separatorsToUnix(toRelativeSourceName(project, cmxFile, relativeCmi));
-        VirtualFile contentRoot = Platform.findORPackageJsonContentRoot(project);
-        VirtualFile sourceFile = contentRoot == null ? null : contentRoot.findFileByRelativePath(relativeSource);
-
-        if (sourceFile == null && contentRoot != null) {
-            relativeSource = relativeSource.replace(".re", ".ml");
-            sourceFile = contentRoot.findFileByRelativePath(relativeSource);
-        }
-
-        return sourceFile;
-    }
-
-    @Nullable
-    private static Path pathFromSource(@NotNull Project project, @NotNull VirtualFile baseRoot, @NotNull Path relativeBuildPath,
-                                       @NotNull VirtualFile sourceFile, boolean useCmt) {
-        Path baseRootPath = FileSystems.getDefault().getPath(baseRoot.getPath());
-        Path relativePath;
-        try {
-            relativePath = baseRootPath.relativize(new File(sourceFile.getPath()).toPath());
-        } catch (IllegalArgumentException e) {
-            // Path can't be relative
-            return null;
-        }
-
-        Path relativeParent = relativePath.getParent();
-        if (relativeParent != null) {
-            relativeBuildPath = relativeBuildPath.resolve(relativeParent);
-        }
-
-        String namespace = ServiceManager.getService(project, Bucklescript.class).getNamespace(sourceFile);
-        return relativeBuildPath.resolve(sourceFile.getNameWithoutExtension() + (namespace.isEmpty() ? "" : "-" + namespace) + (useCmt ? ".cmt" : ".cmi"));
-    }
-
-    @Nullable
-    public static VirtualFile fromSource(@NotNull Project project, @NotNull VirtualFile baseRoot, @NotNull Path relativeBuildPath,
-                                         @NotNull VirtualFile sourceFile, boolean useCmt) {
-        Path path = pathFromSource(project, baseRoot, relativeBuildPath, sourceFile, useCmt);
-        if (path == null) {
-            return null;
-        }
-        String relativeCmiPath = separatorsToUnix(path.toString());
-        return baseRoot.findFileByRelativePath(relativeCmiPath);
-    }
-
-    @NotNull
-    private static String separatorsToUnix(@NotNull String path) {
-        return path.replace('\\', '/');
     }
 }
