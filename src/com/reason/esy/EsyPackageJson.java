e58a3a6acd7db3e950bce7d81d20ed23bd7ccbe6
Hervé Giraud <giraud.contact@yahoo.fr>
2020-05-02 09:43:25 +0200

    Update to new 2020 API

--- src/com/reason/esy/EsyPackageJson.java
+++ src/com/reason/esy/EsyPackageJson.java
@@ -16,6 +16,8 @@ import com.intellij.util.indexing.FileContentImpl;
 import com.reason.ide.files.EsyPackageJsonFileType;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
+
 public class EsyPackageJson {
 
     private EsyPackageJson() {}
@@ -25,8 +27,12 @@ public class EsyPackageJson {
         if (virtualFile.isDirectory()) {
             return false;
         }
-        FileContent fileContent = FileContentImpl.createByFile(virtualFile);
-        return createFilePattern().accepts(fileContent);
+        try {
+            FileContent fileContent = FileContentImpl.createByFile(virtualFile);
+            return createFilePattern().accepts(fileContent);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static FileType getFileType() {
