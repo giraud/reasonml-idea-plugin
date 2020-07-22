1c35971b0cf4a6723040497874fd6fa0139f99d6
Hervé Giraud <giraud.contact@yahoo.fr>
2020-07-22 12:39:15 +0200

    Update file stubs version

--- idea-plugin/src/com/reason/lang/core/stub/type/OclFileStubElementType.java
+++ idea-plugin/src/com/reason/lang/core/stub/type/OclFileStubElementType.java
@@ -13,7 +13,7 @@ import com.reason.lang.ocaml.OclLanguage;
 import org.jetbrains.annotations.NotNull;
 
 public class OclFileStubElementType extends IStubFileElementType<OclFileStub> {
-    private static final int VERSION = 2;
+    private static final int VERSION = 3;
     public static final IStubFileElementType<OclFileStub> INSTANCE = new OclFileStubElementType();
 
     private OclFileStubElementType() {
