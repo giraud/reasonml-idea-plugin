82a380ceda2537798803bbf383b6d437d2331f6d
John Pucciarelli <john.pucciarelli@gmail.com>
2020-08-16 15:49:47 -0400

    Fixed tests.

--- idea-plugin/tests/com/reason/bs/BsPlatformTest.java
+++ idea-plugin/tests/com/reason/bs/BsPlatformTest.java
@@ -12,6 +12,7 @@ import java.util.Optional;
 import static com.reason.bs.BsConstants.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class BsPlatformTest {
@@ -31,28 +32,48 @@ public class BsPlatformTest {
         MockVirtualFile mockBsConfigFile = MockVirtualFile.file(BS_CONFIG_FILENAME);
         MockVirtualFile mockBsPlatformDirectory = MockVirtualFile.dir(BS_PLATFORM_DIRECTORY_NAME);
         MockVirtualFile mockNodeModulesDirectory = MockVirtualFile.dir("node_modules", mockBsPlatformDirectory);
-        MockVirtualFile mockSourceFile = MockVirtualFile.dir(MOCK_ROOT, mockBsConfigFile, mockNodeModulesDirectory);
-        Optional<VirtualFile> bsPlatformDirectory = BsPlatform.findBsPlatformDirectory(mockProject, mockSourceFile);
+        MockVirtualFile mockSourceDirectory = MockVirtualFile.dir(MOCK_ROOT, mockBsConfigFile, mockNodeModulesDirectory);
+        Optional<VirtualFile> bsPlatformDirectory = BsPlatform.findBsPlatformDirectory(mockProject, mockSourceDirectory);
         assertTrue(bsPlatformDirectory.isPresent());
         assertEquals(mockBsPlatformDirectory, bsPlatformDirectory.get());
     }
 
     @Test
     public void testFindBsbExecutable_platformSpecificBinary() {
+        MockVirtualFile mockSourceFile = MockVirtualFile.file("mock-source-file.bs");
+        MockVirtualFile mockSourceDirectory = MockVirtualFile.dir("src", mockSourceFile);
+
         MockVirtualFile mockBsbExecutable = MockVirtualFile.file(BSB_EXECUTABLE_NAME + ".exe");
         MockVirtualFile mockPlatformDirectory = MockVirtualFile.dir(getOsBsPrefix(), mockBsbExecutable);
         MockVirtualFile mockBsPlatformDirectory = MockVirtualFile.dir(BS_PLATFORM_DIRECTORY_NAME, mockPlatformDirectory);
-        Optional<VirtualFile> bsbExecutable = BsPlatform.findBsbExecutable(mockBsPlatformDirectory);
+        MockVirtualFile mockNodeModulesDirectory = MockVirtualFile.dir("node_modules", mockBsPlatformDirectory);
+
+        MockVirtualFile mockBsConfigFile = MockVirtualFile.file(BS_CONFIG_FILENAME);
+
+        MockVirtualFile mockRootDirectory = MockVirtualFile.dir(MOCK_ROOT, mockBsConfigFile, mockNodeModulesDirectory, mockSourceDirectory);
+        when(mockProject.getBasePath()).thenReturn(mockRootDirectory.getPath());
+
+        Optional<VirtualFile> bsbExecutable = BsPlatform.findBsbExecutable(mockProject, mockSourceFile);
         assertTrue(bsbExecutable.isPresent());
         assertEquals(mockBsbExecutable, bsbExecutable.get());
     }
 
     @Test
     public void testFindBsbExecutable_platformAgnosticWrapper() {
+        MockVirtualFile mockSourceFile = MockVirtualFile.file("mock-source-file.bs");
+        MockVirtualFile mockSourceDirectory = MockVirtualFile.dir("src", mockSourceFile);
+
         String bsbWrapperFilename = BSB_EXECUTABLE_NAME + BsPlatform.getOsBinaryWrapperExtension();
         MockVirtualFile mockBsbExecutableWrapper = MockVirtualFile.file(bsbWrapperFilename);
         MockVirtualFile mockBsPlatformDirectory = MockVirtualFile.dir(BS_PLATFORM_DIRECTORY_NAME, mockBsbExecutableWrapper);
-        Optional<VirtualFile> bsbExecutableWrapper = BsPlatform.findBsbExecutable(mockBsPlatformDirectory);
+        MockVirtualFile mockNodeModulesDirectory = MockVirtualFile.dir("node_modules", mockBsPlatformDirectory);
+
+        MockVirtualFile mockBsConfigFile = MockVirtualFile.file(BS_CONFIG_FILENAME);
+
+        MockVirtualFile mockRootDirectory = MockVirtualFile.dir(MOCK_ROOT, mockBsConfigFile, mockNodeModulesDirectory, mockSourceDirectory);
+        when(mockProject.getBasePath()).thenReturn(mockRootDirectory.getPath());
+
+        Optional<VirtualFile> bsbExecutableWrapper = BsPlatform.findBsbExecutable(mockProject, mockBsPlatformDirectory);
         assertTrue(bsbExecutableWrapper.isPresent());
         assertEquals(mockBsbExecutableWrapper, bsbExecutableWrapper.get());
     }
