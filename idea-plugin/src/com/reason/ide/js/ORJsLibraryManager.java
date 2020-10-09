da8ce33d7f196bf0d9e8385f839aed4ee659b1d5
Hervé Giraud <giraud.contact@yahoo.fr>
2020-10-09 08:09:29 +0200

    Change after merge

--- idea-plugin/src/com/reason/ide/js/ORJsLibraryManager.java
+++ idea-plugin/src/com/reason/ide/js/ORJsLibraryManager.java
@@ -5,13 +5,11 @@ import static com.intellij.util.ArrayUtilRt.EMPTY_STRING_ARRAY;
 import static com.intellij.webcore.libraries.ScriptingLibraryModel.LibraryLevel.PROJECT;
 
 import com.intellij.lang.javascript.library.JSLibraryManager;
-import com.intellij.openapi.Disposable;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
-import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.pointers.VirtualFilePointer;
 import com.intellij.openapi.vfs.pointers.VirtualFilePointerListener;
@@ -21,9 +19,7 @@ import com.reason.Log;
 import com.reason.bs.BsConfig;
 import com.reason.bs.BsConfigReader;
 import com.reason.ide.ORProjectManager;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import org.jetbrains.annotations.NotNull;
 
 public class ORJsLibraryManager implements StartupActivity, DumbAware {
@@ -36,7 +32,7 @@ public class ORJsLibraryManager implements StartupActivity, DumbAware {
     DumbService.getInstance(project).smartInvokeLater(() -> runActivityLater(project));
   }
 
-  private void runActivityLater(@NotNull Project project) {
+  private void runActivityLater(Project project) {
     LOG.info("run Js library manager");
 
     Optional<VirtualFile> bsConfigFileOptional =
@@ -75,7 +71,7 @@ public class ORJsLibraryManager implements StartupActivity, DumbAware {
     }
   }
 
-  private @NotNull List<VirtualFile> readBsConfigDependencies(
+  private List<VirtualFile> readBsConfigDependencies(
       @NotNull Project project, @NotNull String nodeModulesDir, @NotNull VirtualFile bsConfigFile) {
     List<VirtualFile> result = new ArrayList<>();
 
@@ -84,65 +80,60 @@ public class ORJsLibraryManager implements StartupActivity, DumbAware {
     VirtualFilePointerManager vFilePointerManager = VirtualFilePointerManager.getInstance();
     BsConfig bsConfig = BsConfigReader.read(bsConfigFile);
 
-    JSLibraryManager jsLibraryManager = JSLibraryManager.getInstance(project);
-    Disposable disposable = Disposer.newDisposable(jsLibraryManager, "ORJsLibraryManager");
-    try {
-      for (String dependency : bsConfig.getDependencies()) {
-        String depDirUrl = nodeModulesDir + dependency;
-        VirtualFilePointer dirPointer =
-            vFilePointerManager.create(
-                depDirUrl,
-                disposable,
-                new VirtualFilePointerListener() {
-                  @Override
-                  public void validityChanged(@NotNull VirtualFilePointer[] pointers) {
-                    if (LOG.isDebugEnabled()) {
-                      LOG.debug("validity changed for " + pointers[0]);
-                    }
-                    ScriptingLibraryModel bucklescriptModel =
-                        jsLibraryManager.getLibraryByName(LIB_NAME);
-                    if (bucklescriptModel != null) {
-                      List<VirtualFile> changedSources =
-                          new ArrayList<>(bucklescriptModel.getSourceFiles());
-                      if (pointers[0].isValid()) {
-                        VirtualFile dirFile = pointers[0].getFile();
-                        changedSources.add(dirFile);
-                        if (dirFile != null) {
-                          VirtualFile bsConfigDepFile = dirFile.findChild("bsconfig.json");
-                          if (bsConfigDepFile != null) {
-                            changedSources.addAll(
-                                readBsConfigDependencies(project, nodeModulesDir, bsConfigDepFile));
-                          }
+    for (String dependency : bsConfig.getDependencies()) {
+      String depDirUrl = nodeModulesDir + dependency;
+      VirtualFilePointer dirPointer =
+          vFilePointerManager.create(
+              depDirUrl,
+              project,
+              new VirtualFilePointerListener() {
+                @Override
+                public void validityChanged(@NotNull VirtualFilePointer[] pointers) {
+                  if (LOG.isDebugEnabled()) {
+                    LOG.debug("validity changed for " + pointers[0]);
+                  }
+                  JSLibraryManager jsLibraryManager = JSLibraryManager.getInstance(project);
+                  ScriptingLibraryModel bucklescriptModel =
+                      jsLibraryManager.getLibraryByName(LIB_NAME);
+                  if (bucklescriptModel != null) {
+                    List<VirtualFile> changedSources =
+                        new ArrayList<>(bucklescriptModel.getSourceFiles());
+                    if (pointers[0].isValid()) {
+                      VirtualFile dirFile = pointers[0].getFile();
+                      changedSources.add(dirFile);
+                      if (dirFile != null) {
+                        VirtualFile bsConfigDepFile = dirFile.findChild("bsconfig.json");
+                        if (bsConfigDepFile != null) {
+                          changedSources.addAll(
+                              readBsConfigDependencies(project, nodeModulesDir, bsConfigDepFile));
                         }
-                        jsLibraryManager.updateLibrary(
-                            LIB_NAME,
-                            LIB_NAME,
-                            changedSources.toArray(new VirtualFile[0]),
-                            EMPTY_ARRAY,
-                            EMPTY_STRING_ARRAY);
-                        WriteCommandAction.runWriteCommandAction(
-                            project, (Runnable) jsLibraryManager::commitChanges);
-                      } else {
-                        System.out.println("removed " + pointers[0]);
                       }
+                      jsLibraryManager.updateLibrary(
+                          LIB_NAME,
+                          LIB_NAME,
+                          changedSources.toArray(new VirtualFile[0]),
+                          EMPTY_ARRAY,
+                          EMPTY_STRING_ARRAY);
+                      WriteCommandAction.runWriteCommandAction(
+                          project, (Runnable) jsLibraryManager::commitChanges);
+                    } else {
+                      System.out.println("removed " + pointers[0]);
                     }
                   }
-                });
+                }
+              });
 
-        VirtualFile dirFile = dirPointer.getFile();
-        if (dirFile != null) {
-          result.add(dirFile);
+      VirtualFile dirFile = dirPointer.getFile();
+      if (dirFile != null) {
+        result.add(dirFile);
 
-          if (dirFile.isValid()) {
-            VirtualFile bsConfigDepFile = dirFile.findChild("bsconfig.json");
-            if (bsConfigDepFile != null) {
-              result.addAll(readBsConfigDependencies(project, nodeModulesDir, bsConfigDepFile));
-            }
+        if (dirFile.isValid()) {
+          VirtualFile bsConfigDepFile = dirFile.findChild("bsconfig.json");
+          if (bsConfigDepFile != null) {
+            result.addAll(readBsConfigDependencies(project, nodeModulesDir, bsConfigDepFile));
           }
         }
       }
-    } finally {
-      disposable.dispose();
     }
 
     return result;
