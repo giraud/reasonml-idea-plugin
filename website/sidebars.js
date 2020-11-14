962455117b814b96a510dc9088e8a67cab1ff87b
Pooch <john.pucciarelli@gmail.com>
2020-11-14 12:40:24 -0500

    Created Website (#281)
    * Created documentation website.
    
    * Better theme.
    
    * Moved docs into website.
    
    * Fixed package version.
    
    * Continuing work on website.
    
    * Began setting up deployment.
    
    * Responsive features.
    
    * Fixed navbar link activeBasePath for 'Documentation'
    
    * Fixed vertical padding on homepage.
    
    * Removed dead code. Updated footer links.
    
    * Feature images now responsive.

---
+++ website/sidebars.js
@@ -0,0 +1,6 @@
+module.exports = {
+  someSidebar: {
+    'Usage Guides': ['get-started', 'project-types', 'bucklescript-support', 'esy-support'],
+    'Contributing': ['architecture', 'plugin-development'],
+  },
+};
---
+++ website/src/theme/Hero/index.js
@@ -0,0 +1,62 @@
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+
+import styles from "./styles.module.scss";
+
+function Hero() {
+    const context = useDocusaurusContext();
+    const { siteConfig = {} } = context;
+
+    return (
+        <header id="hero" className={clsx("hero", styles.hero)}>
+            <div className="container">
+                <img
+                    src={useBaseUrl(`img/logo.svg`)}
+                    alt="Logo"
+                    className={styles.logo}
+                />
+                <h1 className="hero__title">{siteConfig.title}</h1>
+                <p className={clsx("hero__subtitle", styles.subtitle)} dangerouslySetInnerHTML={{__html: siteConfig.tagline}} />
+
+                <div className={styles.badges}>
+                  <a href=""><img alt="Build Status" src="https://github.com/reasonml-editor/reasonml-idea-plugin/workflows/Build%20Status/badge.svg" /></a>
+                  <a href="https://plugins.jetbrains.com/plugin/9440-reasonml">
+                    <img alt="JetBrains IntelliJ plugins" src="https://img.shields.io/jetbrains/plugin/d/9440-reasonml.svg" />
+                  </a>
+                  <a href="https://discord.com/channels/713777184996589580">
+                    <img alt="Discord" src="https://img.shields.io/discord/713777184996589580" />
+                  </a>
+                  <a href="https://opensource.org/licenses/MIT">
+                    <img alt="License: MIT" src="https://img.shields.io/badge/License-MIT-yellow.svg" />
+                  </a>
+                </div>
+
+                <div className={styles.buttons}>
+                  <Link
+                      className={clsx(
+                          "button button--primary button--lg",
+                          styles.getStarted
+                      )}
+                      to="https://plugins.jetbrains.com/plugin/9440-reasonml"
+                  >
+                      Download
+                  </Link>
+                  <Link
+                    className={clsx(
+                      "button button--secondary button--lg",
+                      styles.getStarted
+                    )}
+                    to={useBaseUrl("docs/")}
+                  >
+                    Get Started
+                  </Link>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Hero;
