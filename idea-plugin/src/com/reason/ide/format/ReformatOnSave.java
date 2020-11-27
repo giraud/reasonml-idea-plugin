f45a382342b71b054ecbb12b5b1a319cf45dea78
Hervé Giraud <giraud.contact@yahoo.fr>
2020-11-27 14:06:43 +0100

    Add a guard to limit number of consecutive saves

--- idea-plugin/src/com/reason/ide/format/ReformatOnSave.java
+++ idea-plugin/src/com/reason/ide/format/ReformatOnSave.java
@@ -7,6 +7,7 @@ import com.intellij.openapi.command.undo.*;
 import com.intellij.openapi.editor.*;
 import com.intellij.openapi.fileEditor.*;
 import com.intellij.openapi.project.*;
+import com.intellij.openapi.util.*;
 import com.intellij.openapi.vfs.*;
 import com.intellij.psi.*;
 import com.reason.*;
@@ -14,7 +15,7 @@ import com.reason.ide.settings.*;
 import org.jetbrains.annotations.*;
 
 public class ReformatOnSave {
-
+  private static final Key<Integer> REFORMAT_COUNT = new Key<>("reasonml.format.count");
   private static final Log LOG = Log.create("format.auto");
 
   public static void apply(@NotNull Project project, @NotNull Document document) {
@@ -24,8 +25,14 @@ public class ReformatOnSave {
       if (psiFile != null && psiFile.isWritable()) {
         VirtualFile virtualFile = psiFile.getVirtualFile();
         if (virtualFile != null && virtualFile.exists()) {
+          Integer count = psiFile.getUserData(REFORMAT_COUNT);
           if (LOG.isDebugEnabled()) {
-            LOG.debug("Before document saving (" + project.getName() + ", autoSave=true): " + psiFile.getVirtualFile());
+            LOG.debug("Before document saving (" + project.getName() + ", autoSave=true, count=" + count + "): " + psiFile.getVirtualFile());
+          }
+          if (count != null && count > 2) {
+            LOG.warn("   -> Too many saves (" + count + "), auto reformat is cancelled");
+            psiFile.putUserData(REFORMAT_COUNT, 1);
+            return;
           }
 
           String textToReformat = psiFile.getText();
@@ -34,23 +41,29 @@ public class ReformatOnSave {
 
           if (newText == null || textToReformat.equals(newText)) {
             LOG.debug(" -> Text unchanged, abort format");
+            psiFile.putUserData(REFORMAT_COUNT, 1);
           } else {
             ApplicationManagerEx.getApplicationEx()
-                .invokeLater(() ->
-                                 WriteAction.run(() -> {
-                                   CommandProcessor.getInstance()
-                                       .executeCommand(project, () -> {
-                                             document.setText(newText);
-                                             FileDocumentManager.getInstance().saveDocument(document);
-                                             PsiFile newFile = PsiDocumentManager.getInstance(project).getPsiFile(document);
-                                             if (newFile != null) {
-                                               newFile.putUserData(UndoConstants.FORCE_RECORD_UNDO, null);
-                                             }
-                                           },
-                                           "or.reformat",
-                                           "CodeFormatGroup",
-                                           document);
-                                 }));
+                .invokeLater(() -> WriteAction.run(() -> {
+                  PsiFile newFile = PsiDocumentManager.getInstance(project).getPsiFile(document);
+                  if (newFile != null) {
+                    if (document.getText().equals(newText)) {
+                      LOG.debug(" -> Text unchanged, abort format");
+                      newFile.putUserData(REFORMAT_COUNT, 1);
+                    } else {
+                      CommandProcessor.getInstance().executeCommand(project, () -> {
+                            document.setText(newText);
+                            Integer newCount = newFile.getUserData(REFORMAT_COUNT);
+                            newFile.putUserData(REFORMAT_COUNT, newCount == null ? 1 : newCount + 1);
+                            newFile.putUserData(UndoConstants.FORCE_RECORD_UNDO, null);
+                            FileDocumentManager.getInstance().saveDocument(document);
+                          },
+                          "or.reformat",
+                          "CodeFormatGroup",
+                          document);
+                    }
+                  }
+                }));
           }
         }
       }
