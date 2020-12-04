8f5ddce27bdea69e91b05dba4e72086652721002
Hervé Giraud <giraud.contact@yahoo.fr>
2020-12-04 11:20:58 +0100

    #289 - StringLiteralEscaper is not accessible outside Idea

--- idea-plugin/src/com/reason/lang/core/psi/PsiLiteralExpression.java
+++ idea-plugin/src/com/reason/lang/core/psi/PsiLiteralExpression.java
@@ -1,9 +1,10 @@
 package com.reason.lang.core.psi;
 
 import com.intellij.lang.*;
+import com.intellij.openapi.util.*;
 import com.intellij.psi.*;
 import com.intellij.psi.impl.source.tree.*;
-import com.intellij.psi.impl.source.tree.injected.*;
+import com.intellij.psi.impl.source.tree.java.*;
 import com.intellij.psi.tree.*;
 import org.jetbrains.annotations.*;
 
@@ -33,6 +34,35 @@ public class PsiLiteralExpression extends LeafPsiElement implements /*PsiLiteral
   @Override
   public String toString() {
     return "PsiLiteralExpression:" + getText();
+  }
+
+  // Copied from com.intellij.psi.impl.source.tree.injected.StringLiteralEscaper
+  // See https://github.com/reasonml-editor/reasonml-idea-plugin/issues/289
+  public static class StringLiteralEscaper<T extends PsiLanguageInjectionHost> extends LiteralTextEscaper<T> {
+    private int[] outSourceOffsets;
+
+    public StringLiteralEscaper(T host) {
+      super(host);
+    }
+
+    @Override
+    public boolean decode(@NotNull final TextRange rangeInsideHost, @NotNull StringBuilder outChars) {
+      String subText = rangeInsideHost.substring(myHost.getText());
+      outSourceOffsets = new int[subText.length() + 1];
+      return PsiLiteralExpressionImpl.parseStringCharacters(subText, outChars, outSourceOffsets);
+    }
+
+    @Override
+    public int getOffsetInHost(int offsetInDecoded, @NotNull final TextRange rangeInsideHost) {
+      int result = offsetInDecoded < outSourceOffsets.length ? outSourceOffsets[offsetInDecoded] : -1;
+      if (result == -1) return -1;
+      return Math.min(result, rangeInsideHost.getLength()) + rangeInsideHost.getStartOffset();
+    }
+
+    @Override
+    public boolean isOneLine() {
+      return true;
+    }
   }
 
 }
