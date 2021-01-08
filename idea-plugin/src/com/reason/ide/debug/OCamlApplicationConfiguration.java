c5392aec8ca604407417b1f81c5f7ebcfa760ffb
Herve Giraud <hgiraud@axway.com>
2021-01-08 08:40:05 +0100

    update after merge

--- idea-plugin/src/com/reason/ide/debug/OCamlApplicationConfiguration.java
+++ idea-plugin/src/com/reason/ide/debug/OCamlApplicationConfiguration.java
@@ -4,11 +4,13 @@ import com.intellij.execution.*;
 import com.intellij.execution.configurations.*;
 import com.intellij.execution.runners.*;
 import com.intellij.openapi.module.*;
+import com.intellij.openapi.module.Module;
 import com.intellij.openapi.options.*;
 import com.reason.lang.core.psi.*;
 import org.jetbrains.annotations.*;
 
 import java.util.*;
+
 
 public class OCamlApplicationConfiguration
         extends ModuleBasedConfiguration<OCamlModuleBasedConfiguration, PsiModule> {
@@ -18,17 +20,17 @@ public class OCamlApplicationConfiguration
     }
 
     @Override
-    public @Nullable Collection<Module> getValidModules() {
+    public Collection<Module> getValidModules() {
         return null;
     }
 
     @Override
     public @NotNull SettingsEditor<? extends RunConfiguration> getConfigurationEditor() {
         return new CompositeSettingsEditor<RunConfiguration>() {
-          @Override
-          public CompositeSettingsBuilder<RunConfiguration> getBuilder() {
-            return null;
-          }
+            @Override
+            public CompositeSettingsBuilder<RunConfiguration> getBuilder() {
+                return null;
+            }
         };
     }
 
