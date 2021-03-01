33618015a4c5d002309f975ecea4aa900a412208
Hervé Giraud <giraud.contact@yahoo.fr>
2021-03-01 16:47:29 +0100

    fix broken compilation (not gradle)

--- src/com/reason/ide/go/ORDelegatePsiElement.java
+++ src/com/reason/ide/go/ORDelegatePsiElement.java
@@ -7,137 +7,128 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiManager;
 import com.intellij.psi.impl.PsiElementBase;
 import com.reason.lang.core.psi.PsiQualifiedElement;
+
 import javax.swing.*;
+
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public abstract class ORDelegatePsiElement extends PsiElementBase {
-  protected final @NotNull PsiQualifiedElement m_source;
+    protected final @NotNull PsiQualifiedElement m_source;
 
-  protected ORDelegatePsiElement(@NotNull PsiQualifiedElement source) {
-    m_source = source;
-  }
+    protected ORDelegatePsiElement(@NotNull PsiQualifiedElement source) {
+        m_source = source;
+    }
 
-  @Override
-  public @NotNull PsiElement getOriginalElement() {
-    return m_source;
-  }
+    @Override
+    public @NotNull PsiElement getOriginalElement() {
+        return m_source;
+    }
 
-  @Nullable
-  @Override
-  public PsiElement getParent() {
-    return m_source.getParent();
-  }
+    @Override
+    public @Nullable PsiElement getParent() {
+        return m_source.getParent();
+    }
 
-  @Override
-  public @NotNull PsiElement getNavigationElement() {
-    return m_source.getNavigationElement();
-  }
+    @Override
+    public @NotNull PsiElement getNavigationElement() {
+        return m_source.getNavigationElement();
+    }
 
-  @Override
-  @NotNull
-  public Language getLanguage() {
-    return m_source.getLanguage();
-  }
+    @Override
+    public @NotNull Language getLanguage() {
+        return m_source.getLanguage();
+    }
 
-  @Override
-  public PsiElement @NotNull [] getChildren() {
-    return m_source.getChildren();
-  }
+    @Override
+    public @NotNull PsiElement[] getChildren() {
+        return m_source.getChildren();
+    }
 
-  @Override
-  @Nullable
-  public PsiElement getFirstChild() {
-    return m_source.getFirstChild();
-  }
+    @Override
+    public @Nullable PsiElement getFirstChild() {
+        return m_source.getFirstChild();
+    }
 
-  @Override
-  @Nullable
-  public PsiElement getLastChild() {
-    return m_source.getLastChild();
-  }
+    @Override
+    public @Nullable PsiElement getLastChild() {
+        return m_source.getLastChild();
+    }
 
-  @Override
-  @Nullable
-  public PsiElement getNextSibling() {
-    return m_source.getNextSibling();
-  }
+    @Override
+    public @Nullable PsiElement getNextSibling() {
+        return m_source.getNextSibling();
+    }
 
-  @Override
-  @Nullable
-  public PsiElement getPrevSibling() {
-    return m_source.getPrevSibling();
-  }
+    @Override
+    public @Nullable PsiElement getPrevSibling() {
+        return m_source.getPrevSibling();
+    }
 
-  @Override
-  @Nullable
-  public TextRange getTextRange() {
-    return m_source.getTextRange();
-  }
+    @Override
+    public @Nullable TextRange getTextRange() {
+        return m_source.getTextRange();
+    }
 
-  @Override
-  public int getStartOffsetInParent() {
-    return m_source.getStartOffsetInParent();
-  }
+    @Override
+    public int getStartOffsetInParent() {
+        return m_source.getStartOffsetInParent();
+    }
 
-  @Override
-  public int getTextLength() {
-    return m_source.getTextLength();
-  }
+    @Override
+    public int getTextLength() {
+        return m_source.getTextLength();
+    }
 
-  @Override
-  @Nullable
-  public PsiElement findElementAt(int offset) {
-    return m_source.findElementAt(offset);
-  }
+    @Override
+    public @Nullable PsiElement findElementAt(int offset) {
+        return m_source.findElementAt(offset);
+    }
 
-  @Override
-  public int getTextOffset() {
-    return m_source.getTextOffset();
-  }
+    @Override
+    public int getTextOffset() {
+        return m_source.getTextOffset();
+    }
 
-  @Override
-  @Nullable
-  @NonNls
-  public String getText() {
-    return m_source.getText();
-  }
+    @Override
+    @NonNls
+    public @Nullable String getText() {
+        return m_source.getText();
+    }
 
-  @NotNull
-  @Override
-  public TextRange getTextRangeInParent() {
-    return m_source.getTextRangeInParent();
-  }
+    @Override
+    public @NotNull TextRange getTextRangeInParent() {
+        return m_source.getTextRangeInParent();
+    }
 
-  @Override
-  public char @NotNull [] textToCharArray() {
-    return m_source.textToCharArray();
-  }
+    @Override
+    public @NotNull char[] textToCharArray() {
+        return m_source.textToCharArray();
+    }
 
-  @Override
-  public boolean textContains(char c) {
-    return m_source.textContains(c);
-  }
+    @Override
+    public boolean textContains(char c) {
+        return m_source.textContains(c);
+    }
 
-  @Override
-  @Nullable
-  public ASTNode getNode() {
-    return m_source.getNode();
-  }
+    @Override
+    public @Nullable ASTNode getNode() {
+        return m_source.getNode();
+    }
 
-  @Override
-  public @Nullable Icon getIcon(final int flags) {
-    return m_source.getIcon(flags);
-  }
+    @Override
+    public @Nullable Icon getIcon(final int flags) {
+        return m_source.getIcon(flags);
+    }
 
-  @Override
-  public PsiManager getManager() {
-    return m_source.getManager();
-  }
+    @Override
+    public PsiManager getManager() {
+        return m_source.getManager();
+    }
 
-  @Override
-  public boolean isPhysical() {
-    return false;
-  }
+    @Override
+    public boolean isPhysical() {
+        return false;
+    }
 }
--- src/com/reason/ide/highlight/DuneSyntaxHighlighter.java
+++ src/com/reason/ide/highlight/DuneSyntaxHighlighter.java
@@ -1,83 +1,67 @@
 package com.reason.ide.highlight;
 
-import static com.intellij.openapi.editor.colors.TextAttributesKey.createTextAttributesKey;
-import static com.intellij.psi.TokenType.BAD_CHARACTER;
+import com.intellij.lexer.*;
+import com.intellij.openapi.editor.*;
+import com.intellij.openapi.editor.colors.*;
+import com.intellij.openapi.fileTypes.*;
+import com.intellij.psi.tree.*;
+import com.reason.lang.dune.*;
+import org.jetbrains.annotations.*;
 
-import com.intellij.lexer.FlexAdapter;
-import com.intellij.lexer.Lexer;
-import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
-import com.intellij.openapi.editor.HighlighterColors;
-import com.intellij.openapi.editor.colors.TextAttributesKey;
-import com.intellij.openapi.fileTypes.SyntaxHighlighterBase;
-import com.intellij.psi.tree.IElementType;
-import com.reason.lang.dune.DuneLexer;
-import com.reason.lang.dune.DuneTypes;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-import org.jetbrains.annotations.NotNull;
+import static com.intellij.openapi.editor.colors.TextAttributesKey.*;
+import static com.intellij.psi.TokenType.*;
 
 public class DuneSyntaxHighlighter extends SyntaxHighlighterBase {
 
-  public static final TextAttributesKey PARENS_ =
-      createTextAttributesKey("DUNE_PAREN", DefaultLanguageHighlighterColors.PARENTHESES);
-  public static final TextAttributesKey COMMENT_ =
-      createTextAttributesKey("DUNE_COMMENT", DefaultLanguageHighlighterColors.BLOCK_COMMENT);
-  public static final TextAttributesKey STANZAS_ =
-      createTextAttributesKey("DUNE_STANZA", DefaultLanguageHighlighterColors.FUNCTION_DECLARATION);
-  public static final TextAttributesKey FIELDS_ =
-      createTextAttributesKey("DUNE_FIELD", DefaultLanguageHighlighterColors.KEYWORD);
-  public static final TextAttributesKey OPTIONS_ =
-      createTextAttributesKey("DUNE_OPTION", DefaultLanguageHighlighterColors.LABEL);
-  public static final TextAttributesKey STRING_ =
-      createTextAttributesKey("DUNE_STRING", DefaultLanguageHighlighterColors.STRING);
-  public static final TextAttributesKey ATOM_ =
-      createTextAttributesKey("DUNE_ATOM", DefaultLanguageHighlighterColors.CONSTANT);
-  public static final TextAttributesKey VAR_ =
-      createTextAttributesKey("DUNE_VAR", DefaultLanguageHighlighterColors.LOCAL_VARIABLE);
-  private static final TextAttributesKey BAD_CHAR_ =
-      createTextAttributesKey("DUNE_BAD_CHARACTER", HighlighterColors.BAD_CHARACTER);
+    public static final TextAttributesKey PARENS_ =
+            createTextAttributesKey("DUNE_PAREN", DefaultLanguageHighlighterColors.PARENTHESES);
+    public static final TextAttributesKey COMMENT_ =
+            createTextAttributesKey("DUNE_COMMENT", DefaultLanguageHighlighterColors.BLOCK_COMMENT);
+    public static final TextAttributesKey STANZAS_ =
+            createTextAttributesKey("DUNE_STANZA", DefaultLanguageHighlighterColors.FUNCTION_DECLARATION);
+    public static final TextAttributesKey FIELDS_ =
+            createTextAttributesKey("DUNE_FIELD", DefaultLanguageHighlighterColors.KEYWORD);
+    public static final TextAttributesKey OPTIONS_ =
+            createTextAttributesKey("DUNE_OPTION", DefaultLanguageHighlighterColors.LABEL);
+    public static final TextAttributesKey STRING_ =
+            createTextAttributesKey("DUNE_STRING", DefaultLanguageHighlighterColors.STRING);
+    public static final TextAttributesKey ATOM_ =
+            createTextAttributesKey("DUNE_ATOM", DefaultLanguageHighlighterColors.CONSTANT);
+    public static final TextAttributesKey VAR_ =
+            createTextAttributesKey("DUNE_VAR", DefaultLanguageHighlighterColors.LOCAL_VARIABLE);
+    private static final TextAttributesKey BAD_CHAR_ =
+            createTextAttributesKey("DUNE_BAD_CHARACTER", HighlighterColors.BAD_CHARACTER);
 
-  private static final TextAttributesKey[] COMMENT_KEYS = new TextAttributesKey[] {COMMENT_};
-  private static final TextAttributesKey[] PAREN_KEYS = new TextAttributesKey[] {PARENS_};
-  private static final TextAttributesKey[] STANZAS_KEYS = new TextAttributesKey[] {STANZAS_};
-  private static final TextAttributesKey[] FIELDS_KEYS = new TextAttributesKey[] {FIELDS_};
-  private static final TextAttributesKey[] OPTIONS_KEYS = new TextAttributesKey[] {OPTIONS_};
-  private static final TextAttributesKey[] STRING_KEYS = new TextAttributesKey[] {STRING_};
-  private static final TextAttributesKey[] ATOM_KEYS = new TextAttributesKey[] {ATOM_};
-  private static final TextAttributesKey[] VAR_KEYS = new TextAttributesKey[] {VAR_};
-  private static final TextAttributesKey[] BAD_CHAR_KEYS = new TextAttributesKey[] {BAD_CHAR_};
-  private static final TextAttributesKey[] EMPTY_KEYS = new TextAttributesKey[0];
+    private static final TextAttributesKey[] COMMENT_KEYS = new TextAttributesKey[]{COMMENT_};
+    private static final TextAttributesKey[] PAREN_KEYS = new TextAttributesKey[]{PARENS_};
+    private static final TextAttributesKey[] STANZAS_KEYS = new TextAttributesKey[]{STANZAS_};
+    private static final TextAttributesKey[] FIELDS_KEYS = new TextAttributesKey[]{FIELDS_};
+    private static final TextAttributesKey[] OPTIONS_KEYS = new TextAttributesKey[]{OPTIONS_};
+    private static final TextAttributesKey[] STRING_KEYS = new TextAttributesKey[]{STRING_};
+    private static final TextAttributesKey[] ATOM_KEYS = new TextAttributesKey[]{ATOM_};
+    private static final TextAttributesKey[] VAR_KEYS = new TextAttributesKey[]{VAR_};
+    private static final TextAttributesKey[] BAD_CHAR_KEYS = new TextAttributesKey[]{BAD_CHAR_};
+    private static final TextAttributesKey[] EMPTY_KEYS = new TextAttributesKey[0];
 
-  @NotNull
-  @Override
-  public Lexer getHighlightingLexer() {
-    return new FlexAdapter(new DuneLexer(DuneTypes.INSTANCE));
-  }
-
-  @NotNull
-  @Override
-  public TextAttributesKey @NotNull [] getTokenHighlights(@NotNull IElementType tokenType) {
-    if (tokenType.equals(DuneTypes.INSTANCE.LPAREN)
-        || tokenType.equals(DuneTypes.INSTANCE.RPAREN)) {
-      return PAREN_KEYS;
-    } else if (tokenType.equals(DuneTypes.INSTANCE.COMMENT)) {
-      return COMMENT_KEYS;
-    } else if (tokenType.equals(DuneTypes.INSTANCE.STRING)) {
-      return STRING_KEYS;
-    } else if (tokenType.equals(DuneTypes.INSTANCE.ATOM)) {
-      return ATOM_KEYS;
-    } else if (BAD_CHARACTER.equals(tokenType)) {
-      return BAD_CHAR_KEYS;
+    @Override
+    public @NotNull Lexer getHighlightingLexer() {
+        return new FlexAdapter(new DuneLexer(DuneTypes.INSTANCE));
     }
 
-    return EMPTY_KEYS;
-  }
+    @Override
+    public @NotNull TextAttributesKey[] getTokenHighlights(@NotNull IElementType tokenType) {
+        if (tokenType.equals(DuneTypes.INSTANCE.LPAREN) || tokenType.equals(DuneTypes.INSTANCE.RPAREN)) {
+            return PAREN_KEYS;
+        } else if (tokenType.equals(DuneTypes.INSTANCE.COMMENT)) {
+            return COMMENT_KEYS;
+        } else if (tokenType.equals(DuneTypes.INSTANCE.STRING)) {
+            return STRING_KEYS;
+        } else if (tokenType.equals(DuneTypes.INSTANCE.ATOM)) {
+            return ATOM_KEYS;
+        } else if (BAD_CHARACTER.equals(tokenType)) {
+            return BAD_CHAR_KEYS;
+        }
 
-  @NotNull
-  private static Set<IElementType> of(IElementType... types) {
-    Set<IElementType> result = new HashSet<>();
-    Collections.addAll(result, types);
-    return result;
-  }
+        return EMPTY_KEYS;
+    }
 }
