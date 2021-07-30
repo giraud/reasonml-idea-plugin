a1066bd4d6287851246d5fcbff8bba136e777f4c
Hervé Giraud <giraud.contact@yahoo.fr>
2021-07-30 14:26:13 +0200

    Fix pluginVerifier warnings

--- src/icons/ORIcons.java
+++ src/icons/ORIcons.java
@@ -1,10 +1,10 @@
 package icons;
 
-import com.intellij.icons.AllIcons;
+import com.intellij.icons.*;
 
 import javax.swing.*;
 
-import static com.intellij.openapi.util.IconLoader.getIcon;
+import static com.intellij.openapi.util.IconLoader.*;
 
 /*
  * https://www.jetbrains.org/intellij/sdk/docs/reference_guide/work_with_icons_and_images.html
@@ -19,10 +19,10 @@ import static com.intellij.openapi.util.IconLoader.getIcon;
 public class ORIcons {
     // TOOL WINDOW
 
-    public static final Icon BUCKLESCRIPT_TOOL = getIcon("/icons/bucklescriptTool.svg");
-    public static final Icon RESCRIPT_TOOL = getIcon("/icons/rescriptTool.svg");
-    public static final Icon DUNE_TOOL = getIcon("/icons/duneTool.svg");
-    public static final Icon ESY_TOOL = getIcon("/icons/esyTool.svg");
+    public static final Icon BUCKLESCRIPT_TOOL = getIcon("/icons/bucklescriptTool.svg", ORIcons.class);
+    public static final Icon RESCRIPT_TOOL = getIcon("/icons/rescriptTool.svg", ORIcons.class);
+    public static final Icon DUNE_TOOL = getIcon("/icons/duneTool.svg", ORIcons.class);
+    public static final Icon ESY_TOOL = getIcon("/icons/esyTool.svg", ORIcons.class);
 
     // GUTTER
 
@@ -31,40 +31,40 @@ public class ORIcons {
 
     // OTHER
 
-    public static final Icon OCAML = getIcon("/icons/ocamlLogo.svg");
-    public static final Icon DUNE = getIcon("/icons/duneLogo.svg");
-    public static final Icon ESY = getIcon("/icons/esy.svg");
+    public static final Icon OCAML = getIcon("/icons/ocamlLogo.svg", ORIcons.class);
+    public static final Icon DUNE = getIcon("/icons/duneLogo.svg", ORIcons.class);
+    //public static final Icon ESY = getIcon("/icons/esy.svg", ORIcons.class);
 
-    public static final Icon BS_FILE = getIcon("/icons/bsFile.svg");
-    public static final Icon ESY_FILE = getIcon("/icons/esyFile.svg");
-    public static final Icon RML_FILE = getIcon("/icons/reFile.svg");
-    public static final Icon RML_INTERFACE_FILE = getIcon("/icons/reiFile.svg");
-    public static final Icon NS_FILE = getIcon("/icons/resFile.svg");
-    public static final Icon NS_INTERFACE_FILE = getIcon("/icons/resiFile.svg");
-    public static final Icon OCL_FILE = getIcon("/icons/mlFile.svg");
-    public static final Icon OCL_INTERFACE_FILE = getIcon("/icons/mliFile.png");
-    public static final Icon DUNE_FILE = getIcon("/icons/duneFile.svg");
+    public static final Icon BS_FILE = getIcon("/icons/bsFile.svg", ORIcons.class);
+    public static final Icon ESY_FILE = getIcon("/icons/esyFile.svg", ORIcons.class);
+    public static final Icon RML_FILE = getIcon("/icons/reFile.svg", ORIcons.class);
+    public static final Icon RML_INTERFACE_FILE = getIcon("/icons/reiFile.svg", ORIcons.class);
+    public static final Icon NS_FILE = getIcon("/icons/resFile.svg", ORIcons.class);
+    public static final Icon NS_INTERFACE_FILE = getIcon("/icons/resiFile.svg", ORIcons.class);
+    public static final Icon OCL_FILE = getIcon("/icons/mlFile.svg", ORIcons.class);
+    public static final Icon OCL_INTERFACE_FILE = getIcon("/icons/mliFile.png", ORIcons.class);
+    public static final Icon DUNE_FILE = getIcon("/icons/duneFile.svg", ORIcons.class);
 
-    public static final Icon RML_FILE_MODULE = getIcon("/icons/reasonRed.svg");
-    public static final Icon RML_FILE_MODULE_INTERFACE = getIcon("/icons/reasonBlue.svg");
-    public static final Icon OCL_FILE_MODULE = getIcon("/icons/ocamlLogo.svg");
-    public static final Icon OCL_FILE_MODULE_INTERFACE = getIcon("/icons/ocamlBlue.png");
+    public static final Icon RML_FILE_MODULE = getIcon("/icons/reasonRed.svg", ORIcons.class);
+    public static final Icon RML_FILE_MODULE_INTERFACE = getIcon("/icons/reasonBlue.svg", ORIcons.class);
+    public static final Icon OCL_FILE_MODULE = getIcon("/icons/ocamlLogo.svg", ORIcons.class);
+    public static final Icon OCL_FILE_MODULE_INTERFACE = getIcon("/icons/ocamlBlue.png", ORIcons.class);
 
-    public static final Icon RML_BLUE = getIcon("/icons/reasonBlue.svg");
-    public static final Icon RML_YELLOW = getIcon("/icons/reasonYellow.svg");
+    public static final Icon RML_BLUE = getIcon("/icons/reasonBlue.svg", ORIcons.class);
+    public static final Icon RML_YELLOW = getIcon("/icons/reasonYellow.svg", ORIcons.class);
 
-    public static final Icon OCL_MODULE = getIcon("/icons/ocamlModule.svg");
-    public static final Icon OCL_SDK = getIcon("/icons/ocamlSdk.svg");
-    public static final Icon OCL_BLUE_FILE = getIcon("/icons/ocamlBlue.png");
-    public static final Icon OCL_GREEN_FILE = getIcon("/icons/ocamlGreen.png");
+    public static final Icon OCL_MODULE = getIcon("/icons/ocamlModule.svg", ORIcons.class);
+    public static final Icon OCL_SDK = getIcon("/icons/ocamlSdk.svg", ORIcons.class);
+    //public static final Icon OCL_BLUE_FILE = getIcon("/icons/ocamlBlue.png", ORIcons.class);
+    public static final Icon OCL_GREEN_FILE = getIcon("/icons/ocamlGreen.png", ORIcons.class);
 
-    public static final Icon TYPE = getIcon("/icons/type.svg");
-    public static final Icon VARIANT = getIcon("/icons/variant.svg");
-    public static final Icon ANNOTATION = AllIcons.Nodes.Annotationtype;
+    public static final Icon TYPE = getIcon("/icons/type.svg", ORIcons.class);
+    public static final Icon VARIANT = getIcon("/icons/variant.svg", ORIcons.class);
+    //public static final Icon ANNOTATION = AllIcons.Nodes.Annotationtype;
 
-    public static final Icon INNER_MODULE = getIcon("/icons/innerModule.svg");
-    public static final Icon INNER_MODULE_INTF = getIcon("/icons/innerModuleIntf.svg");
-    public static final Icon MODULE_TYPE = getIcon("/icons/javaModuleType.svg");
+    public static final Icon INNER_MODULE = getIcon("/icons/innerModule.svg", ORIcons.class);
+    public static final Icon INNER_MODULE_INTF = getIcon("/icons/innerModuleIntf.svg", ORIcons.class);
+    public static final Icon MODULE_TYPE = getIcon("/icons/javaModuleType.svg", ORIcons.class);
     public static final Icon FUNCTOR = AllIcons.Nodes.Artifact;
     public static final Icon LET = AllIcons.Nodes.Variable;
     public static final Icon VAL = AllIcons.Nodes.Variable;
@@ -83,9 +83,12 @@ public class ORIcons {
     public static final Icon OVERLAY_MANDATORY = AllIcons.Ide.ErrorPoint;
     public static final Icon OVERLAY_EXECUTE = AllIcons.Nodes.RunnableMark;
 
-    public static final Icon RESET = getIcon("/icons/reset.svg");
+    public static final Icon RESET = getIcon("/icons/reset.svg", ORIcons.class);
 
     // REPL
 
     public static final Icon REPL = AllIcons.Nodes.Console;
+
+    private ORIcons() {
+    }
 }
