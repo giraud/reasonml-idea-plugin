591e802f1c6a82caf4037bae1ae0be305cf3078b
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2021-09-14 21:27:28 +0200

    Adding Scope: Code or Comment for Live templates

--- src/com/reason/ide/template/OCamlContentType.java
+++ src/com/reason/ide/template/OCamlContentType.java
@@ -2,6 +2,9 @@ package com.reason.ide.template;
 
 import com.intellij.codeInsight.template.TemplateActionContext;
 import com.intellij.codeInsight.template.TemplateContextType;
+import com.intellij.psi.*;
+import com.intellij.psi.util.*;
+import com.reason.lang.ocaml.*;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -13,9 +16,51 @@ public class OCamlContentType extends TemplateContextType {
         super("OCAML", "Ocaml");
     }
 
+    protected OCamlContentType(String name) {
+        super("OCAML."+name, name, OCamlContentType.class);
+    }
+
     @Override
     public boolean isInContext(@NotNull TemplateActionContext templateActionContext) {
         final String name = templateActionContext.getFile().getName();
         return name.endsWith(".ml") || name.endsWith(".mli");
+    }
+
+    // Code-only
+    private static final class OCamlCodeTemplates extends ScopeTemplates {
+        public OCamlCodeTemplates() {
+            super("Code", false);
+        }
+    }
+    // Comments only
+    private static final class OCamlCommentTemplates extends ScopeTemplates {
+        public OCamlCommentTemplates() {
+            super("Comment", true);
+        }
+    }
+
+    private static class ScopeTemplates extends OCamlContentType {
+        private final boolean comments;
+
+        protected ScopeTemplates(String name, boolean comments) {
+            super(name);
+            this.comments = comments;
+        }
+
+        @Override
+        public boolean isInContext(@NotNull TemplateActionContext templateActionContext) {
+            if(!super.isInContext(templateActionContext)) return false;
+
+            // RmlContextType - copy of the 6 following lines
+            // with RmlLanguage => OclLanguage
+            PsiFile file = templateActionContext.getFile();
+            int offset = templateActionContext.getStartOffset();
+            if (!PsiUtilCore.getLanguageAtOffset(file, offset).isKindOf(OclLanguage.INSTANCE)) {
+                return false;
+            }
+
+            PsiElement element = file.findElementAt(offset);
+            return this.comments == (element instanceof PsiComment);
+        }
     }
 }
