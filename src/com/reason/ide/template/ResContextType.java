ac61319dd9d108634a6320d97c21777e0889e0dd
Hervé Giraud <giraud.contact@yahoo.fr>
2021-09-14 08:43:38 +0200

    add Rescript to live templates

--- src/com/reason/ide/template/ResContextType.java
+++ src/com/reason/ide/template/ResContextType.java
@@ -3,11 +3,11 @@ package com.reason.ide.template;
 import com.intellij.codeInsight.template.*;
 import com.intellij.psi.*;
 import com.intellij.psi.util.*;
-import com.reason.lang.reason.*;
+import com.reason.lang.rescript.*;
 import org.jetbrains.annotations.*;
 
-public abstract class RmlContextType extends TemplateContextType {
-    RmlContextType(@NotNull String id, @NotNull String presentableName, @Nullable Class<? extends TemplateContextType> baseContextType) {
+public abstract class ResContextType extends TemplateContextType {
+    ResContextType(@NotNull String id, @NotNull String presentableName, @Nullable Class<? extends TemplateContextType> baseContextType) {
         super(id, presentableName, baseContextType);
     }
 
@@ -15,7 +15,7 @@ public abstract class RmlContextType extends TemplateContextType {
     public boolean isInContext(@NotNull TemplateActionContext context) {
         PsiFile file = context.getFile();
         int offset = context.getStartOffset();
-        if (!PsiUtilCore.getLanguageAtOffset(file, offset).isKindOf(RmlLanguage.INSTANCE)) {
+        if (!PsiUtilCore.getLanguageAtOffset(file, offset).isKindOf(ResLanguage.INSTANCE)) {
             return false;
         }
 
@@ -29,9 +29,9 @@ public abstract class RmlContextType extends TemplateContextType {
 
     protected abstract boolean isInContext(PsiElement element);
 
-    public static class Generic extends RmlContextType {
+    public static class Generic extends ResContextType {
         protected Generic() {
-            super("REASON_CODE", "Reason", EverywhereContextType.class);
+            super("RESCRIPT_CODE", "Rescript", EverywhereContextType.class);
         }
 
         @Override
