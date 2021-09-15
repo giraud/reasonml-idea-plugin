c72df940b98ce4c690db6786c27f34bfb4761324
Quentin Ra <54904135+QuentinRa@users.noreply.github.com>
2021-09-15 13:36:47 +0200

    Adding Scope: Code or Comment for Live templates (#345)
    * Adding Scope: Code or Comment for Live templates

--- src/com/reason/ide/template/OCamlContentType.java
+++ src/com/reason/ide/template/OCamlContentType.java
@@ -2,6 +2,9 @@ package com.reason.ide.template;
 
 import com.intellij.codeInsight.template.TemplateActionContext;
 import com.intellij.codeInsight.template.TemplateContextType;
+import com.intellij.psi.*;
+import com.intellij.psi.util.*;
+import com.reason.lang.ocaml.*;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -13,9 +16,56 @@ public class OCamlContentType extends TemplateContextType {
         super("OCAML", "Ocaml");
     }
 
+    protected OCamlContentType(String name) {
+        super("OCAML."+name, name, OCamlContentType.class);
+    }
+
     @Override
     public boolean isInContext(@NotNull TemplateActionContext templateActionContext) {
         final String name = templateActionContext.getFile().getName();
         return name.endsWith(".ml") || name.endsWith(".mli");
+    }
+
+    // Code-only
+    private static final class OCamlExpressionTemplates extends ScopeTemplates {
+        public OCamlExpressionTemplates() {
+            super("Expression", false);
+        }
+    }
+    // Comments only
+    private static final class OCamlCommentTemplates extends ScopeTemplates {
+        public OCamlCommentTemplates() {
+            super("Comment", true);
+        }
+    }
+
+    private static class ScopeTemplates extends OCamlContentType {
+        private final boolean myOnComment;
+
+        /**
+         * @param name name of the scope, capitalized
+         * @param onComment if true, then "isContext" is checking that we are inside a Comment,
+         *                 otherwise, "isContext" is checking that we aren't inside a Comment
+         */
+        protected ScopeTemplates(String name, boolean onComment) {
+            super(name);
+            myOnComment = onComment;
+        }
+
+        @Override
+        public boolean isInContext(@NotNull TemplateActionContext templateActionContext) {
+            if(!super.isInContext(templateActionContext)) return false;
+
+            // RmlContextType - copy of the 6 following lines
+            // with RmlLanguage => OclLanguage
+            PsiFile file = templateActionContext.getFile();
+            int offset = templateActionContext.getStartOffset();
+            if (!PsiUtilCore.getLanguageAtOffset(file, offset).isKindOf(OclLanguage.INSTANCE)) {
+                return false;
+            }
+
+            PsiElement element = file.findElementAt(offset);
+            return myOnComment == (element instanceof PsiComment);
+        }
     }
 }
