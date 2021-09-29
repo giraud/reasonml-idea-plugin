49e3b7a8abac04134272968847dce5dcb2eada6c
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2021-09-29 16:29:37 +0200

    extracting code to call a process in ProcessUtils

--- src/com/reason/comp/dune/OcamlFormatProcess.java
+++ src/com/reason/comp/dune/OcamlFormatProcess.java
@@ -1,17 +1,13 @@
 package com.reason.comp.dune;
 
-import com.intellij.execution.*;
 import com.intellij.execution.configurations.*;
 import com.intellij.openapi.project.*;
 import com.intellij.openapi.vfs.*;
 import jpsplugin.com.reason.*;
 import org.jetbrains.annotations.*;
 
-import java.io.*;
 import java.util.*;
 import java.util.concurrent.atomic.*;
-
-import static jpsplugin.com.reason.Platform.*;
 
 public class OcamlFormatProcess {
     private static final Log LOG = Log.create("ocamlformat.process");
@@ -26,47 +22,8 @@ public class OcamlFormatProcess {
     public @NotNull String format(@NotNull VirtualFile file, @NotNull String textToFormat) {
         GeneralCommandLine commandLine = new OCamlFormatCommandLine(myProject, "ocamlformat").addParameters(file).create(file, myConfigurationWarning);
         if (commandLine != null && !textToFormat.isEmpty()) {
-            Process fmt = null;
-            try {
-                fmt = commandLine.createProcess();
-                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(fmt.getOutputStream(), UTF8));
-                BufferedReader reader = new BufferedReader(new InputStreamReader(fmt.getInputStream(), UTF8));
-                BufferedReader errReader = new BufferedReader(new InputStreamReader(fmt.getErrorStream(), UTF8));
-
-                writer.write(textToFormat);
-                writer.flush();
-                writer.close();
-
-                Streams.waitUntilReady(reader, errReader);
-
-                StringBuilder msgBuffer = new StringBuilder();
-                if (!errReader.ready()) {
-                    final boolean[] empty = {true};
-                    reader
-                            .lines()
-                            .forEach(line -> {
-                                if (empty[0]) {
-                                    empty[0] = false;
-                                } else {
-                                    msgBuffer.append('\n');
-                                }
-                                msgBuffer.append(line);
-                            });
-                    String newText = msgBuffer.toString();
-                    if (!newText.isEmpty()) { // additional protection
-                        return newText;
-                    }
-                } else {
-                    errReader.lines().forEach(line -> msgBuffer.append(line).append('\n'));
-                    LOG.warn(StringUtil.trimLastCR(msgBuffer.toString()));
-                }
-            } catch (IOException | RuntimeException | ExecutionException e) {
-                LOG.warn(e);
-            } finally {
-                if (fmt != null) {
-                    fmt.destroyForcibly();
-                }
-            }
+            String output = ProcessUtils.parseOutputFromCommandLine(commandLine, LOG, textToFormat);
+            if (output != null) return output;
         }
 
         return textToFormat;
--- src/jpsplugin/com/reason/ProcessUtils.java
+++ src/jpsplugin/com/reason/ProcessUtils.java
@@ -7,17 +7,32 @@ import org.jetbrains.annotations.*;
 import java.io.*;
 import java.nio.charset.*;
 
-// todo: this was extracted from OcamlFormatProcess
+import static jpsplugin.com.reason.Platform.UTF8;
+
 public class ProcessUtils {
+
+    public static @Nullable String parseOutputFromCommandLine(@NotNull GeneralCommandLine cli, @NotNull Log logger) {
+        return parseOutputFromCommandLine(cli, logger, null);
+    }
 
     /**
      * Extracting that was in "com.reason.comp.dune.OcamlFormatProcess".
-     * Parse the output a command and return it, or Log an error and return null
+     * Parse the output a command and return it, or Log an error and return null.
+     *
+     * Make take some input for the command.
      */
-    public static @Nullable String parseOutputFromCommandLine(@NotNull GeneralCommandLine cli, @NotNull Log logger) {
+    public static @Nullable String parseOutputFromCommandLine(
+            @NotNull GeneralCommandLine cli, @NotNull Log logger, @Nullable String inputText) {
         Process fmt = null;
         try {
             fmt = cli.createProcess();
+            // process input, if submitted
+            if (inputText != null) {
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(fmt.getOutputStream(), UTF8));
+                writer.write(inputText);
+                writer.flush();
+                writer.close();
+            }
             BufferedReader reader = new BufferedReader(new InputStreamReader(fmt.getInputStream(), StandardCharsets.UTF_8));
             BufferedReader errReader = new BufferedReader(new InputStreamReader(fmt.getErrorStream(), StandardCharsets.UTF_8));
 
@@ -45,6 +60,7 @@ public class ProcessUtils {
                 logger.warn(StringUtil.trimLastCR(msgBuffer.toString()));
             }
         } catch (IOException | RuntimeException | ExecutionException e) {
+            System.out.println(e.getMessage());
             logger.warn(e);
         } finally {
             if (fmt != null) {
@@ -53,6 +69,4 @@ public class ProcessUtils {
         }
         return null;
     }
-
-
 }
