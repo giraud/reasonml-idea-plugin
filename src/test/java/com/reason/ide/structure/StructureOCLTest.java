697e599a8d5c54dd60192e674e36812217e0769a
Hervé Giraud <giraud.contact@yahoo.fr>
2023-05-03 08:14:23 +0200

    update goto dialog

--- src/test/java/com/reason/ide/structure/StructureOCLTest.java
+++ src/test/java/com/reason/ide/structure/StructureOCLTest.java
@@ -73,7 +73,7 @@ public class StructureOCLTest extends ORBasePlatformTestCase {
         StructureViewModel model = new ORStructureViewModel(a);
 
         TreeElement i = model.getRoot().getChildren()[0];
-        assertPresentation("I", "", ORIcons.MODULE_TYPE, i.getPresentation());
+        assertPresentation("I", "A.I", ORIcons.MODULE_TYPE, i.getPresentation());
         TreeElement x = i.getChildren()[0];
         assertPresentation("x", "bool", ORIcons.VAL, x.getPresentation());
     }
@@ -85,7 +85,7 @@ public class StructureOCLTest extends ORBasePlatformTestCase {
         StructureViewModel model = new ORStructureViewModel(b);
 
         TreeElement e = model.getRoot().getChildren()[0];
-        assertPresentation("X", "", ORIcons.INNER_MODULE, e.getPresentation());
+        assertPresentation("X", "B.X", ORIcons.INNER_MODULE, e.getPresentation());
         TreeElement ee = e.getChildren()[0];
         assertPresentation("A.S", "", ORIcons.MODULE_TYPE, ee.getPresentation());
     }
@@ -97,7 +97,7 @@ public class StructureOCLTest extends ORBasePlatformTestCase {
         StructureViewModel model = new ORStructureViewModel(b);
 
         TreeElement e = model.getRoot().getChildren()[0];
-        assertPresentation("X", "", ORIcons.INNER_MODULE, e.getPresentation());
+        assertPresentation("X", "B.X", ORIcons.INNER_MODULE, e.getPresentation());
         TreeElement ee = e.getChildren()[0];
         assertPresentation("A.Vcs.Branch", "", ORIcons.MODULE_TYPE, ee.getPresentation());
     }
