920471a8af1f98ed6e8426cf03f80d17850d7440
Hervé Giraud <giraud.contact@yahoo.fr>
2023-11-22 09:21:44 +0100

    Fix language injection and macro delimiter (437)

--- src/test/java/com/reason/lang/reason/MacroParsingTest.java
+++ src/test/java/com/reason/lang/reason/MacroParsingTest.java
@@ -1,9 +1,6 @@
 package com.reason.lang.reason;
 
 import com.intellij.openapi.util.*;
-import com.intellij.psi.*;
-import com.intellij.psi.util.*;
-import com.reason.lang.core.psi.*;
 import com.reason.lang.core.psi.impl.*;
 import org.junit.*;
 
@@ -11,39 +8,39 @@ import org.junit.*;
 public class MacroParsingTest extends RmlParsingTestCase {
     @Test
     public void test_basic() {
-        RPsiLet expression = firstOfType(parseCode("let _ = [%raw \"xxx\"]"), RPsiLet.class);
+        RPsiMacro e = firstOfType(parseCode("let _ = [%raw \"xxx\"]"), RPsiMacro.class);
 
-        PsiElement macro = expression.getBinding().getFirstChild();
-        assertInstanceOf(macro, RPsiMacro.class);
-
-        RPsiMacroBody rawMacroBody = PsiTreeUtil.findChildOfType(macro, RPsiMacroBody.class);
-        assertEquals("%raw", PsiTreeUtil.findChildOfType(macro, RPsiMacroName.class).getText());
-        assertEquals("\"xxx\"", rawMacroBody.getText());
-        assertEquals(new TextRange(1, 4), rawMacroBody.getMacroTextRange());
+        assertEquals("%raw", e.getName());
+        assertEquals("\"xxx\"", e.getContent().getText());
+        assertEquals(new TextRange(1, e.getContent().getTextLength() - 1), e.getContent().getMacroTextRange()); // exclude "
     }
 
     @Test
-    public void test_rootRaw() {
-        RPsiMacro e = firstOfType(parseCode("%raw \"xxx\";"), RPsiMacro.class);
+    public void test_root_raw() {
+        RPsiMacro e = firstOfType(parseCode("%raw \"import * from react\";"), RPsiMacro.class);
 
         assertTrue(e.isRoot());
-        assertEquals("\"xxx\"", e.getContent().getText());
+        assertEquals("%raw", e.getName());
+        assertEquals("\"import * from react\"", e.getContent().getText());
+        assertEquals(new TextRange(1, e.getContent().getTextLength() - 1), e.getContent().getMacroTextRange()); // exclude "
     }
 
     @Test
-    public void test_multiLine() {
-        RPsiLet expression = firstOfType(parseCode("let _ = [%raw {|function (a) {}|}]"), RPsiLet.class);
+    public void test_multi_line() {
+        RPsiMacro e = firstOfType(parseCode("""
+                let _ = [%raw {|
+                    function (a) {
+                    }
+                |}]
+                """), RPsiMacro.class);
 
-        RPsiMacro macro = (RPsiMacro) expression.getBinding().getFirstChild();
-
-        assertEquals("%raw", macro.getName());
-        assertEquals("{|function (a) {}|}", macro.getContent().getText());
-        assertEquals(new TextRange(2, 17), macro.getContent().getMacroTextRange()); // exclude {| |}
+        assertEquals("%raw", e.getName());
+        assertEquals("{|\n    function (a) {\n    }\n|}", e.getContent().getText());
+        assertEquals(new TextRange(2, e.getContent().getTextLength() - 2), e.getContent().getMacroTextRange()); // exclude {| |}
     }
 
-
     @Test
-    public void test_GH_xxx() {
+    public void test_GH_436() {
         RPsiMacro e = firstOfType(parseCode("""
                 module GetOperatorBasesQuery = [%graphql {|
                   query SchedGetOperatorBases {
@@ -59,5 +56,6 @@ public class MacroParsingTest extends RmlParsingTestCase {
 
         assertEquals("%graphql", e.getName());
         assertEquals("{|\n  query SchedGetOperatorBases {\n    operatorBases {\n      baseNid\n      name\n      utcOffset\n    }\n  }\n|}", e.getContent().getText());
+        assertEquals(new TextRange(2, e.getContent().getTextLength() - 2), e.getContent().getMacroTextRange()); // exclude {| |}
     }
 }
