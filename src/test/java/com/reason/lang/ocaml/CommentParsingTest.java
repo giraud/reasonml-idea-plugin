2b781657438c1f35d5139900231e743ca00ed8cc
Hervé Giraud <giraud.contact@yahoo.fr>
2024-07-05 12:14:12 +0200

    '"' in comments confuses comment recognition (#469)

--- src/test/java/com/reason/lang/ocaml/CommentParsingTest.java
+++ src/test/java/com/reason/lang/ocaml/CommentParsingTest.java
@@ -1,14 +1,14 @@
 package com.reason.lang.ocaml;
 
 import com.intellij.psi.*;
+import com.intellij.psi.util.*;
+import com.reason.ide.files.*;
 import com.reason.lang.*;
+import com.reason.lang.core.*;
+import com.reason.lang.core.psi.*;
 import org.junit.*;
 
-public class CommentParsingTest extends BaseParsingTestCase {
-    public CommentParsingTest() {
-        super("", "ml", new OclParserDefinition());
-    }
-
+public class CommentParsingTest extends OclParsingTestCase {
     @Test
     public void test_constant() {
         PsiComment e = firstOfType(parseCode("(* *)"), PsiComment.class);
@@ -21,5 +21,30 @@ public class CommentParsingTest extends BaseParsingTestCase {
         PsiComment e = firstOfType(parseCode("(* \"this is a string *)\" *)"), PsiComment.class);
 
         assertEquals("(* \"this is a string *)\" *)", e.getText());
+    }
+
+
+    // GH: https://github.com/giraud/reasonml-idea-plugin/issues/469
+    @Test
+    public void test_double_quotes() {
+        FileBase e = parseCode("""
+                let _ = '"' in
+                (* '"' *)
+                let _ = '"' in
+                (* '"' *)
+                """);
+
+        PsiElement e0 = e.getFirstChild();
+        assertInstanceOf(e0, RPsiLet.class);
+        assertEquals("let _ = '\"'", e0.getText());
+        PsiElement e1 = ORUtil.nextSibling(ORUtil.nextSibling(e0));
+        assertInstanceOf(e1, PsiComment.class);
+        assertEquals("(* '\"' *)", e1.getText());
+        PsiElement e2 = ORUtil.nextSibling(e1);
+        assertInstanceOf(e2, RPsiLet.class);
+        assertEquals("let _ = '\"'", e2.getText());
+        PsiElement e3 = ORUtil.nextSibling(ORUtil.nextSibling(e2));
+        assertInstanceOf(e3, PsiComment.class);
+        assertEquals("(* '\"' *)", e3.getText());
     }
 }
