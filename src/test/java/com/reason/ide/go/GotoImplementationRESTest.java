5a826d7da739ffbfc45582b51fbad33fdc2bc01e
Hervé Giraud <giraud.contact@yahoo.fr>
2024-12-11 09:23:43 +0100

    Update tests

--- src/test/java/com/reason/ide/go/GotoImplementationRESTest.java
+++ src/test/java/com/reason/ide/go/GotoImplementationRESTest.java
@@ -1,22 +1,24 @@
 package com.reason.ide.go;
 
-import com.intellij.psi.*;
-import com.intellij.testFramework.fixtures.*;
-import com.reason.ide.*;
-import com.reason.ide.files.*;
-import com.reason.lang.core.psi.*;
-import org.junit.*;
-import org.junit.runner.*;
-import org.junit.runners.*;
+import org.junit.Test;
+import com.intellij.codeInsight.navigation.GotoTargetHandler;
+import com.intellij.psi.PsiElement;
+import com.intellij.testFramework.fixtures.CodeInsightTestUtil;
+import com.reason.ide.ORBasePlatformTestCase;
+import com.reason.ide.files.FileBase;
+import com.reason.lang.core.psi.RPsiQualifiedPathElement;
 
 public class GotoImplementationRESTest extends ORBasePlatformTestCase {
     @Test
     public void should_goto_implementation() {
         configureCode("A.resi", "let x: int");
         configureCode("A.res", "let x = 1");
-        FileBase ref = configureCode("B.res", "let y = A.x<caret>");
+        FileBase b = configureCode("B.res", "let y = A.x<caret>");
 
-        PsiElement[] targets = CodeInsightTestUtil.gotoImplementation(myFixture.getEditor(), ref).targets;
+        myFixture.openFileInEditor(b.getVirtualFile());
+        GotoTargetHandler.GotoData gotoData = CodeInsightTestUtil.gotoImplementation(myFixture.getEditor(), b);
+
+        PsiElement[] targets = gotoData.targets;
         assertSize(1, targets);
         assertEquals("A.x", ((RPsiQualifiedPathElement) targets[0]).getQualifiedName());
         assertEquals("A.res", targets[0].getContainingFile().getName());
